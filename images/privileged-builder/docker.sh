ef2874779cf1079f978a9c267f63f1df4010255e
Tomas Tomecek <ttomecek@redhat.com>
2014-10-24 17:57:14 +0200

    resolve error with exceeded loopback devices

--- images/privileged-builder/docker.sh
+++ images/privileged-builder/docker.sh
@@ -84,6 +84,34 @@ popd >/dev/null
 # delete it so that docker can start.
 rm -rf /var/run/docker.pid
 
+
+# https://github.com/jpetazzo/dind/issues/19#issuecomment-48859883
+# thank you
+ensure_loop(){
+  num="$1"
+  dev="/dev/loop$num"
+  if test -b "$dev"; then
+    echo "$dev is a usable loop device."
+    return 0
+  fi
+
+  echo "Attempting to create $dev for docker ..."
+  if ! mknod -m660 $dev b 7 $num; then
+    echo "Failed to create $dev!" 1>&2
+    return 3
+  fi
+
+  return 0
+}
+
+LOOP_A=$(losetup -f)
+LOOP_A=${LOOP_A#/dev/loop}
+LOOP_B=$(expr $LOOP_A + 1)
+
+ensure_loop $LOOP_A
+ensure_loop $LOOP_B
+
+
 docker -d &
 
 exec dock
