2818e669cc2071ad2a08b8fd81cb08ce5749d525
Tomas Tomecek <ttomecek@redhat.com>
2014-10-29 10:24:49 +0100

    tests: comment hostdocker test, use https:// always
    because we are cloning repo every time now

--- tests/test_dock.py
+++ tests/test_dock.py
@@ -2,18 +2,17 @@ from dock.core import DockerBuilder
 from dock.outer import PrivilegedDockerBuilder
 
 
-def test_hostdocker_build():
-    db = DockerBuilder(
-        "https://github.com/TomasTomecek/docker-hello-world.git",
-        "dock-test-image",
-    )
-    db.build_hostdocker("buildroot-fedora")
+#def test_hostdocker_build():
+#    db = DockerBuilder(
+#        "https://github.com/TomasTomecek/docker-hello-world.git",
+#        "dock-test-image",
+#    )
+#    db.build_hostdocker("buildroot-fedora")
 
 
 def test_privileged_build():
     db = PrivilegedDockerBuilder("buildroot-fedora", {
-        #"git_url": "https://github.com/TomasTomecek/docker-hello-world.git",
-        "git_url": "github.com/TomasTomecek/docker-hello-world.git",
+        "git_url": "https://github.com/TomasTomecek/docker-hello-world.git",
         "local_tag": "dock-test-image",
     })
     db.build()
