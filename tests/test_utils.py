2057adfa9e5f7360aac9c1865c6c1b3a7987ed49
Tomas Tomecek <ttomecek@redhat.com>
2015-02-02 11:28:37 +0100

    add more util tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,7 @@
+import os
+import docker
 from dock.util import split_repo_img_name_tag, join_repo_img_name_tag, get_baseimage_from_dockerfile, \
-    join_repo_img_name, join_img_name_tag
+    join_repo_img_name, join_img_name_tag, wait_for_command, clone_git_repo
 
 
 TEST_DATA = [
@@ -54,6 +56,20 @@ def test_join_img_tag():
         assert result == chain
 
 
-# FIXME
-# def test_get_baseimg_from_df():
-#     assert 'fedora:latest' == get_baseimage_from_dockerfile('https://github.com/TomasTomecek/docker-hello-world.git')
+def test_wait_for_command():
+    d = docker.Client()
+    logs_gen = d.pull("busybox:latest", stream=True)
+    assert wait_for_command(logs_gen) is not None
+
+
+def test_clone_git_repo(tmpdir):
+    tmpdir_path = str(tmpdir.realpath())
+    clone_git_repo('https://github.com/TomasTomecek/docker-hello-world.git', tmpdir_path)
+    assert os.path.isdir(os.path.join(tmpdir_path, '.git'))
+
+
+def test_get_baseimg_from_df(tmpdir):
+    tmpdir_path = str(tmpdir.realpath())
+    clone_git_repo('https://github.com/TomasTomecek/docker-hello-world.git', tmpdir_path)
+    base_img = get_baseimage_from_dockerfile(tmpdir_path)
+    assert base_img.startswith('fedora')
