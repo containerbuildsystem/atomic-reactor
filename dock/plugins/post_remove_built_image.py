91752ef65454233e44805a00a34fead9f50648b8
Tomas Tomecek <ttomecek@redhat.com>
2015-02-04 14:04:46 +0100

    remove built img plugin: dont fail when no img was built

--- dock/plugins/post_remove_built_image.py
+++ dock/plugins/post_remove_built_image.py
@@ -21,4 +21,8 @@ class GarbageCollectionPlugin(PostBuildPlugin):
         super(GarbageCollectionPlugin, self).__init__(tasker, workflow)
 
     def run(self):
-        self.tasker.remove_image(self.workflow.builder.image_id, force=True)
+        image = self.workflow.builder.image_id
+        if not image:
+            self.log.error("no built image, nothing to remove")
+            return
+        self.tasker.remove_image(image, force=True)
