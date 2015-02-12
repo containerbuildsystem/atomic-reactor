af71ab0f10d95fdcb3bd4af2527d4d8e3799555f
Tomas Tomecek <ttomecek@redhat.com>
2015-02-12 11:56:07 +0100

    cli: some required options were not actually mandatory

--- dock/cli/main.py
+++ dock/cli/main.py
@@ -81,13 +81,13 @@ class CLI(object):
         build_parser = subparsers.add_parser('build', help='build image')
         build_parser.set_defaults(func=cli_build_image)
         build_parser.add_argument("--json", action="store", help="path to build json")
-        build_parser.add_argument("--build-image", action='store', required=True,
+        build_parser.add_argument("--build-image", action='store',
                                   help="name of build image to use "
                                   "(build image type has to match method)")
-        build_parser.add_argument("--image", action='store', required=True,
+        build_parser.add_argument("--image", action='store',
                                   help="name under the image will be accessible")
         build_parser.add_argument("--git-url", action='store', metavar="URL",
-                                  required=True, help="URL to git repo")
+                                  help="URL to git repo")
         build_parser.add_argument("--git-path", action='store',
                                   help="path to Dockerfile within git repo (default is ./)")
         build_parser.add_argument("--git-commit", action='store',
