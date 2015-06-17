9be74a251b8cd9226ae50802ee8f8b70b4989cd8
Tomas Tomecek <ttomecek@redhat.com>
2015-06-17 10:43:10 +0200

    1.3.6

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def _install_requirements():
 
 setup(
     name='dock',
-    version='1.3.5',
+    version='1.3.6',
     description=DESCRIPTION,
     author='Tomas Tomecek',  # FIXME: when under project atomic
     author_email='ttomecek@redhat.com',
