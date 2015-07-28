7080809f0556f5b29f250ca75f15e43a7767245b
Jiri Popelka <jpopelka@redhat.com>
2015-07-28 15:08:11 +0200

    Mount CONTAINER_SHARE_PATH volume with 'Z' mode to relabel file objects.
    see
    https://github.com/projectatomic/atomic-reactor/issues/206#issuecomment-125289844
    
    Support for custom mode was added to docker-py in 1.3.0 with
    https://github.com/docker/docker-py/commit/7dd762539196eaade8da183d7588627c51d5a479
    so we have to check whether the support is there beforehand.

--- atomic_reactor/core.py
+++ atomic_reactor/core.py
@@ -104,6 +104,15 @@ class BuildContainerFactory(object):
                 json.dump(build_json, fp)
         # else we do nothing
 
+    @staticmethod
+    def _volume_bind_understands_mode():
+        # docker.utils.convert_volume_binds() understands 'mode' since docker-py-1.3.0
+        # returns ['/a/:/b/:rw'] with docker-py < 1.3.0 and ['/a/:/b/:ro,Z'] with >= 1.3.0
+        bind = docker.utils.convert_volume_binds({'/a/': {'bind': '/b/', 'mode': 'ro,Z'}})
+        if bind and len(bind[0].split(':')) > 2 and bind[0].split(':')[2] == 'ro,Z':
+            return True
+        return False
+
     def build_image_dockerhost(self, build_image, json_args_path):
         """
         Build docker image within a build image using docker from host (mount docker socket inside container).
@@ -130,13 +139,18 @@ class BuildContainerFactory(object):
         volume_bindings = {
             DOCKER_SOCKET_PATH: {
                 'bind': DOCKER_SOCKET_PATH,
-                'ro': True,
             },
             json_args_path: {
                 'bind': CONTAINER_SHARE_PATH,
-                'rw': True,
             },
         }
+
+        if self._volume_bind_understands_mode():
+            volume_bindings[DOCKER_SOCKET_PATH]['mode'] = 'ro'
+            volume_bindings[json_args_path]['mode'] = 'rw,Z'
+        else:
+            volume_bindings[DOCKER_SOCKET_PATH]['ro'] = True
+            volume_bindings[json_args_path]['rw'] = True
 
         logger.debug('build json mounted in container: %s',
                 open(os.path.join(json_args_path, BUILD_JSON)).read())
@@ -166,12 +180,23 @@ class BuildContainerFactory(object):
         self._check_build_input(build_image, json_args_path)
         self._obtain_source_from_path_if_needed(json_args_path, CONTAINER_SHARE_PATH)
 
+        volume_bindings = {
+            json_args_path: {
+                'bind': CONTAINER_SHARE_PATH,
+            },
+        }
+
+        if self._volume_bind_understands_mode():
+            volume_bindings[json_args_path]['mode'] = 'rw,Z'
+        else:
+            volume_bindings[json_args_path]['rw'] = True
+
         logger.debug('build json mounted in container: %s',
                 open(os.path.join(json_args_path, BUILD_JSON)).read())
         container_id = self.tasker.run(
             ImageName.parse(build_image),
             create_kwargs={'volumes': [json_args_path]},
-            start_kwargs={'binds': {json_args_path: {'bind': CONTAINER_SHARE_PATH, 'rw': True}},
+            start_kwargs={'binds': volume_bindings,
                           'privileged': True}
         )
 
