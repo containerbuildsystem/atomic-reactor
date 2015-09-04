8bd43565b01025d42eea0633109f31453c520723
Tim Waugh <twaugh@redhat.com>
2015-09-04 17:33:41 +0100

    Avoid requiring pygit2 (fixes #305)
    It's still quite useful for testing the bump_release plugin so is used
    only for that.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM fedora:latest
 # e2fsprogs -- docker @ F20 wants it
-RUN yum -y install docker-io git python-docker-py python-setuptools python-pygit2 e2fsprogs koji python-pip python-backports-lzma osbs gssproxy
+RUN yum -y install docker-io git python-docker-py python-setuptools e2fsprogs koji python-pip python-backports-lzma osbs gssproxy
 RUN mkdir /tmp/atomic-reactor
 ADD . /tmp/atomic-reactor
 RUN cd /tmp/atomic-reactor && python setup.py install
--- tests/plugins/test_bump_release.py
+++ tests/plugins/test_bump_release.py
@@ -10,23 +10,8 @@ from __future__ import unicode_literals
 
 try:
     import pygit2
-    NO_PYGIT2 = False
 except ImportError:
-    NO_PYGIT2 = True
-    import inspect
-    import os
-    import sys
-
-    # Find our mocked pygit2 module
-    import tests.pygit2 as pygit2
-    mock_pygit2_path = os.path.dirname(os.path.dirname
-                                       (inspect.getfile(pygit2.Remote)))
-    if mock_pygit2_path not in sys.path:
-        sys.path.append(mock_pygit2_path)
-
-    # Now load it properly, the same way the plugin will
-    del pygit2
-    import pygit2
+    pygit2 = None
 
 import pytest
 from atomic_reactor.core import DockerTasker
@@ -99,140 +84,19 @@ class X(object):
     pass
 
 
-class MockRemote(object):
-    def __init__(self, has_set_push_url=False):
-        self.has_set_push_url = has_set_push_url
-
-    def push(self, name, has_set_push_url=False):
-        pass
-
-    @property
-    def push_url(self):
-        return ''
-
-    @push_url.setter
-    def push_url(self, url):
-        if self.has_set_push_url:
-            raise AttributeError("can't set attribute")
-
-    def save(self):
-        if self.has_set_push_url:
-            raise AttributeError("must not call save()")
-
-
-class MockRemotesCollection(object):
-    def __init__(self, has_set_push_url=False):
-        self.has_set_push_url = has_set_push_url
-
-    def create(self, name, url):
-        pass
-
-    def set_push_url(self, name, url):
-        if not self.has_set_push_url:
-            raise AttributeError("no such attribute 'set_push_url'")
-
-    def __getitem__(self, item):
-        return MockRemote(has_set_push_url=self.has_set_push_url)
-
-
-class MockIndex(object):
-    def add(self, name):
-        pass
-
-    def write_tree(self):
-        pass
-
-
-class MockSignature(object):
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
-
-
-class MockCommit(object):
-    def __init__(self):
-        self.author = MockSignature('name', 'email')
-        self.committer = self.author
-        self.hex = '0'
-
-
-class MockBranch(object):
-    def __init__(self, name):
-        self.shorthand = self.name = name
-        self.target = MockCommit()
-        self.upstream = self
-
-    def peel(self):
-        return MockCommit()
-
-
-class MockRepository(object):
-    def __init__(self, *args, **kwargs):
-        has_set_push_url = kwargs.get('has_set_push_url')
-        self.remotes = MockRemotesCollection(has_set_push_url=has_set_push_url)
-        self.index = MockIndex()
-        self.head = flexmock()
-        commit = MockCommit()
-        self.head.should_receive('peel').and_return(commit)
-        self.config = {}
-        self.workdir = '/tmp'
-
-    def create_commit(self, name, author, committer, message, tree, parents):
-        pass
-
-    def create_branch(self, name, commit):
-        return MockBranch(name)
-
-    def lookup_branch(self, name):
-        if name == BRANCH:
-            return MockBranch(name)
-        else:
-            return None
-
-    def get(self, oid):
-        pass
-
-    def checkout(self, refname=None):
-        pass
-
-
-def maybe_mock_pygit2(has_set_push_url=None):
-    flexmock(pygit2.Remote, push=lambda name: None)
-    if NO_PYGIT2 or has_set_push_url is not None:
-        flexmock(pygit2, Signature=MockSignature)
-
-        def do_init_repository(*args, **kwargs):
-            kwargs['has_set_push_url'] = has_set_push_url
-            return MockRepository(*args, **kwargs)
-
-        flexmock(pygit2, init_repository=do_init_repository)
-
-
-class MockedPopen(object):
+class MockedPopen(subprocess.Popen):
     def __init__(self, cmd, *args, **kwargs):
-        self.cmd = cmd
+        # Don't actually push any commits
+        if 'push' in cmd:
+            cmd = ['/bin/echo', 'push faked']
 
-    def __enter__(self, *args, **kwargs):
-        return self
+        # Deal with there being no remote for our branch
+        elif cmd[1:] == ['config', '--get', 'branch.%s.remote' % BRANCH]:
+            cmd = ['/bin/echo', 'origin']
+        elif cmd[1] == 'rev-parse' and cmd[2].startswith('origin/'):
+            cmd[2] = cmd[2][len('origin/'):]
 
-    def __exit__(self, *args, **kwargs):
-        pass
-
-    def kill(self):
-        pass
-
-    def poll(self):
-        return 0
-
-    def wait(self):
-        return 0
-
-    def communicate(self, *args, **kwargs):
-        return ('', '')
-
-
-def fake_Popen(cmd, *args, **kwargs):
-    return MockedPopen(cmd, *args, **kwargs)
+        super(MockedPopen, self).__init__(cmd, *args, **kwargs)
 
 
 def prepare(tmpdir, df_path, git_ref, source=None,
@@ -256,7 +120,7 @@ def prepare(tmpdir, df_path, git_ref, source=None,
     setattr(workflow, 'plugin_failed', build_process_failed)
     workflow.prebuild_results[CheckAndSetRebuildPlugin.key] = is_rebuild
     flexmock(GitSource, get=lambda: os.path.dirname(df_path))
-    flexmock(subprocess, Popen=fake_Popen)
+    flexmock(subprocess, Popen=MockedPopen)
     args = {
         'git_ref': git_ref,
         'author_name': author_name,
@@ -276,8 +140,9 @@ def prepare(tmpdir, df_path, git_ref, source=None,
     return workflow, args, runner
 
 
+@pytest.mark.skipif(pygit2 is None,
+                    reason="pygit2 required for this test")
 def test_bump_release_failed_build(tmpdir):
-    maybe_mock_pygit2()
     with DFWithRelease() as (df_path, commit):
         workflow, args, runner = prepare(tmpdir, df_path, commit,
                                          build_process_failed=True)
@@ -286,8 +151,9 @@ def test_bump_release_failed_build(tmpdir):
         assert open(df_path).readlines() == original_content
 
 
+@pytest.mark.skipif(pygit2 is None,
+                    reason="pygit2 required for this test")
 def test_bump_release_not_rebuild_extra_commits(tmpdir):
-    maybe_mock_pygit2()
     with DFWithRelease() as (df_path, commit):
         workflow, args, runner = prepare(tmpdir, df_path, "wrongcommit",
                                          is_rebuild=False)
@@ -295,16 +161,18 @@ def test_bump_release_not_rebuild_extra_commits(tmpdir):
             runner.run()
 
 
+@pytest.mark.skipif(pygit2 is None,
+                    reason="pygit2 required for this test")
 def test_bump_release_not_rebuild_no_extra_commits(tmpdir):
-    maybe_mock_pygit2()
     with DFWithRelease() as (df_path, commit):
         workflow, args, runner = prepare(tmpdir, df_path, commit,
                                          is_rebuild=False)
         runner.run()
 
 
+@pytest.mark.skipif(pygit2 is None,
+                    reason="pygit2 required for this test")
 def test_bump_release_branch_not_found(tmpdir):
-    maybe_mock_pygit2()
     with DFWithRelease() as (df_path, commit):
         workflow, args, runner = prepare(tmpdir, df_path, commit,
                                          git_commit='wrong')
@@ -312,28 +180,8 @@ def test_bump_release_branch_not_found(tmpdir):
             runner.run()
 
 
-@pytest.mark.parametrize('has_set_push_url', [True, False])
-def test_bump_release_set_push_url(tmpdir, has_set_push_url):
-    """
-    pygit2-0.22 provides a settable Remote.push_url attribute and a
-    save() method, but no RemoteCollection.set_push_url() method.
-
-    pygit2-0.23 provides a RemoteCollection.set_push_url() method, but
-    Remote.push_url is not settable and Remote.save() raises an
-    exception.
-
-    Test that we can cope with either situation.
-
-    """
-
-    maybe_mock_pygit2(has_set_push_url=has_set_push_url)
-    with DFWithRelease() as (df_path, commit):
-        workflow, args, runner = prepare(tmpdir, df_path, commit,
-                                         commit_message='foo')
-        # Test that it doesn't raise an exception
-        runner.run()
-
-
+@pytest.mark.skipif(pygit2 is None,
+                    reason="pygit2 required for this test")
 @pytest.mark.parametrize(('label', 'expected'), [
     # Simple case, no '=' or quotes
     ('LABEL Release 1',
@@ -368,7 +216,6 @@ def test_bump_release_set_push_url(tmpdir, has_set_push_url):
      'LABEL Release=2.1'),
 ])
 def test_bump_release(tmpdir, label, expected):
-    maybe_mock_pygit2()
     with DFWithRelease(label=label) as (df_path, commit):
         workflow, args, runner = prepare(tmpdir, df_path, commit,
                                          commit_message='foo')
@@ -377,24 +224,21 @@ def test_bump_release(tmpdir, label, expected):
         repo = pygit2.init_repository(os.path.dirname(df_path))
 
         # We're on the 'branch' branch
-        assert NO_PYGIT2 or repo.head.name == repo.lookup_branch(BRANCH).name
+        assert repo.head.name == repo.lookup_branch(BRANCH).name
 
         # and one commit ahead of where we were
-        assert NO_PYGIT2 or repo.head.peel().parents[0].hex == commit
+        assert repo.head.peel().parents[0].hex == commit
 
         # Examine metadata for newest commit
         author = repo.head.peel().author
-        assert NO_PYGIT2 or author.name == args['author_name']
-        assert NO_PYGIT2 or author.email == args['author_email']
+        assert author.name == args['author_name']
+        assert author.email == args['author_email']
 
         committer = repo.head.peel().committer
-        assert (NO_PYGIT2 or
-                'committer_name' not in args or
+        assert ('committer_name' not in args or
                 committer.name == args['committer_name'])
-        assert (NO_PYGIT2 or
-                'committer_email' not in args or
+        assert ('committer_email' not in args or
                 committer.email == args['committer_email'])
 
         if 'commit_message' in args:
-            assert (NO_PYGIT2 or
-                    repo.head.peel().message == args['commit_message'])
+            assert repo.head.peel().message.rstrip() == args['commit_message']
