b264df9a0abacc50c6d0428657ef36123970740e
Tomas Tomecek <ttomecek@redhat.com>
2015-11-13 17:05:14 +0100

    add integration test: dockpulp, push

---
+++ integration-tests/__init__.py
@@ -0,0 +1,12 @@
+"""
+Copyright (c) 2015 Red Hat, Inc
+All rights reserved.
+
+This software may be modified and distributed under the terms
+of the BSD license. See the LICENSE file for details.
+"""
+
+from atomic_reactor import set_logging
+
+set_logging(name="atomic_reactor.tests")
+set_logging(name="atomic_reactor")
