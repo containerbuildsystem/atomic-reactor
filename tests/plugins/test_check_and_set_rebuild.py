19e5be05dd035029959fc03c3b0702ec5f057fab
Tim Waugh <twaugh@redhat.com>
2015-12-14 16:22:20 +0000

    Use test class for check_and_set_rebuild

--- tests/plugins/test_check_and_set_rebuild.py
+++ tests/plugins/test_check_and_set_rebuild.py
@@ -27,105 +27,104 @@ class X(object):
     pass
 
 
-def prepare(key, value, set_labels_args=None, set_labels_kwargs=None):
-    if MOCK:
-        mock_docker()
-    tasker = DockerTasker()
-    workflow = DockerBuildWorkflow(SOURCE, "test-image")
-    setattr(workflow, 'builder', X())
-    setattr(workflow.builder, 'image_id', 'asd123')
-    setattr(workflow.builder, 'base_image', ImageName(repo='Fedora', tag='22'))
-    setattr(workflow.builder, 'source', X())
-    setattr(workflow.builder.source, 'path', '/tmp')
-    setattr(workflow.builder.source, 'dockerfile_path', None)
-    expectation = flexmock(OSBS).should_receive('set_labels_on_build_config')
-    if set_labels_args is not None:
-        if set_labels_kwargs is None:
-            set_labels_kwargs = {}
+class TestCheckRebuild(object):
+    def prepare(self, key, value, set_labels_args=None, set_labels_kwargs=None):
+        if MOCK:
+            mock_docker()
+        tasker = DockerTasker()
+        workflow = DockerBuildWorkflow(SOURCE, "test-image")
+        setattr(workflow, 'builder', X())
+        setattr(workflow.builder, 'image_id', 'asd123')
+        setattr(workflow.builder, 'base_image', ImageName(repo='Fedora',
+                                                          tag='22'))
+        setattr(workflow.builder, 'source', X())
+        setattr(workflow.builder.source, 'path', '/tmp')
+        setattr(workflow.builder.source, 'dockerfile_path', None)
+        expectation = (flexmock(OSBS)
+                       .should_receive('set_labels_on_build_config'))
+        if set_labels_args is not None:
+            if set_labels_kwargs is None:
+                set_labels_kwargs = {}
 
-        expectation.with_args(*set_labels_args, **set_labels_kwargs)
+            expectation.with_args(*set_labels_args, **set_labels_kwargs)
 
-    runner = PreBuildPluginsRunner(tasker, workflow,
-                                   [
-                                       {
-                                           'name': CheckAndSetRebuildPlugin.key,
-                                           'args': {
-                                               'label_key': key,
-                                               'label_value': value,
-                                               'url': '',
-                                           },
-                                       }
-                                   ])
-    return workflow, runner
+        runner = PreBuildPluginsRunner(tasker, workflow, [
+            {
+                'name': CheckAndSetRebuildPlugin.key,
+                'args': {
+                    'label_key': key,
+                    'label_value': value,
+                    'url': '',
+                },
+            }
+        ])
+        return workflow, runner
 
+    def test_check_rebuild_no_build_json(self, monkeypatch):
+        workflow, runner = self.prepare('is_autorebuild', 'true')
+        monkeypatch.delenv('BUILD', raising=False)
 
-def test_check_rebuild_no_build_json(monkeypatch):
-    workflow, runner = prepare('is_autorebuild', 'true')
-    monkeypatch.delenv('BUILD', raising=False)
+        with pytest.raises(PluginFailedException):
+            runner.run()
 
-    with pytest.raises(PluginFailedException):
-        runner.run()
+    def test_check_no_buildconfig(self, monkeypatch):
+        key = 'is_autorebuild'
+        value = 'true'
+        workflow, runner = self.prepare(key, value)
+        monkeypatch.setenv("BUILD", json.dumps({
+            "metadata": {
+                "labels": {
+                    key: value,
+                }
+            }
+        }))
 
+        # No buildconfig in metadata
+        with pytest.raises(PluginFailedException):
+            runner.run()
 
-def test_check_no_buildconfig(monkeypatch):
-    key = 'is_autorebuild'
-    value = 'true'
-    workflow, runner = prepare(key, value)
-    monkeypatch.setenv("BUILD", json.dumps({
-        "metadata": {
-            "labels": {
-                key: value,
+    @pytest.mark.parametrize(('namespace'), [None, 'my_namespace'])
+    def test_check_is_not_rebuild(self, namespace, monkeypatch):
+        key = 'is_autorebuild'
+        value = 'true'
+        buildconfig = "buildconfig1"
+        namespace_dict = {}
+        if namespace is not None:
+            namespace_dict["namespace"] = namespace
+
+        workflow, runner = self.prepare(key, value,
+                                        set_labels_args=(buildconfig,
+                                                         {key: value}),
+                                        set_labels_kwargs=namespace_dict)
+
+        build_json = {
+            "metadata": {
+                "labels": {
+                    "buildconfig": buildconfig,
+                    key: "false",
+                }
             }
         }
-    }))
-
-    # No buildconfig in metadata
-    with pytest.raises(PluginFailedException):
-        runner.run()
-
-
-@pytest.mark.parametrize(('namespace'), [None, 'my_namespace'])
-def test_check_is_not_rebuild(namespace, monkeypatch):
-    key = 'is_autorebuild'
-    value = 'true'
-    buildconfig = "buildconfig1"
-    namespace_dict = {}
-    if namespace is not None:
-        namespace_dict["namespace"] = namespace
-
-    workflow, runner = prepare(key, value,
-                               set_labels_args=(buildconfig, {key: value}),
-                               set_labels_kwargs=namespace_dict)
-
-    build_json = {
-        "metadata": {
-            "labels": {
-                "buildconfig": buildconfig,
-                key: "false",
-            }
-        }
-    }
  
-    build_json["metadata"].update(namespace_dict)
-    monkeypatch.setenv("BUILD", json.dumps(build_json))
-    runner.run()
-    assert workflow.prebuild_results[CheckAndSetRebuildPlugin.key] == False
-    assert not is_rebuild(workflow)
+        build_json["metadata"].update(namespace_dict)
+        monkeypatch.setenv("BUILD", json.dumps(build_json))
+        runner.run()
+        assert workflow.prebuild_results[CheckAndSetRebuildPlugin.key] == False
+        assert not is_rebuild(workflow)
 
+    def test_check_is_rebuild(self, monkeypatch):
+        key = 'is_autorebuild'
+        value = 'true'
+        workflow, runner = self.prepare(key, value)
 
-def test_check_is_rebuild(monkeypatch):
-    key = 'is_autorebuild'
-    value = 'true'
-    workflow, runner = prepare(key, value)
-
-    monkeypatch.setenv("BUILD", json.dumps({
-        "metadata": {
-            "labels": {
-                "buildconfig": "buildconfig1",
-                key: value,
+        monkeypatch.setenv("BUILD", json.dumps({
+            "metadata": {
+                "labels": {
+                    "buildconfig": "buildconfig1",
+                    key: value,
+                }
             }
-        }
-    }))
-    runner.run()
-    assert workflow.prebuild_results[CheckAndSetRebuildPlugin.key] == True
-    assert is_rebuild(workflow)
+        }))
+        runner.run()
+        assert workflow.prebuild_results[CheckAndSetRebuildPlugin.key] == True
+        assert is_rebuild(workflow)
