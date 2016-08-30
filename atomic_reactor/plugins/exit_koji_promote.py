9b67397cd548e2cc073f14013a059e15ae6e4d05
Tim Waugh <twaugh@redhat.com>
2016-08-30 15:23:23 +0100

    koji_promote: log more information on error

--- atomic_reactor/plugins/exit_koji_promote.py
+++ atomic_reactor/plugins/exit_koji_promote.py
@@ -515,19 +515,22 @@ class KojiPromotePlugin(ExitPlugin):
             try:
                 extra['container_koji_task_id'] = int(koji_task_id)
             except ValueError:
-                self.log.error("invalid task ID", exc_info=1)
+                self.log.error("invalid task ID %r", koji_task_id, exc_info=1)
 
         fs_result = self.workflow.prebuild_results.get(AddFilesystemPlugin.key)
         if fs_result is not None:
             try:
-                task_id = int(fs_result['filesystem-koji-task-id'])
+                fs_task_id = fs_result['filesystem-koji-task-id']
             except KeyError:
                 self.log.error("%s: expected filesystem-koji-task-id in result",
                                AddFilesystemPlugin.key)
-            except ValueError:
-                self.log.error("invalid task ID", exc_info=1)                
             else:
-                extra['filesystem_koji_task_id'] = task_id
+                try:
+                    task_id = int(fs_task_id)
+                except ValueError:
+                    self.log.error("invalid task ID %r", fs_task_id, exc_info=1)
+                else:
+                    extra['filesystem_koji_task_id'] = task_id
 
         build = {
             'name': component,
