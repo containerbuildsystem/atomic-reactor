a7681130e0fee0393cd980ce1314a30d875e2487
Luiz Carvalho <lucarval@redhat.com>
2016-12-07 12:58:17 -0500

    Avoid osbs.conf collision in import_image

--- atomic_reactor/plugins/post_import_image.py
+++ atomic_reactor/plugins/post_import_image.py
@@ -61,7 +61,8 @@ class ImportImagePlugin(PostBuildPlugin):
         kwargs = {}
 
         # FIXME: remove `openshift_uri` once osbs-client is released
-        osbs_conf = Configuration(openshift_uri=self.url,
+        osbs_conf = Configuration(conf_file=None,
+                                  openshift_uri=self.url,
                                   openshift_url=self.url,
                                   use_auth=self.use_auth,
                                   verify_ssl=self.verify_ssl,
