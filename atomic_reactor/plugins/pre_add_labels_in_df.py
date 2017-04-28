5e1fe8faa5a9496cab2a7f5163748053024f97d4
Vadim Rutkovsky <vrutkovs@redhat.com>
2017-04-28 22:00:59 +0200

    pre_add_labels_in_df: rework add_as_an_alias

--- atomic_reactor/plugins/pre_add_labels_in_df.py
+++ atomic_reactor/plugins/pre_add_labels_in_df.py
@@ -155,22 +155,18 @@ class AddLabelsPlugin(PreBuildPlugin):
                 self.log.warning("requested automatic label %r is not available", lbl)
 
     def add_aliases(self, base_labels, df_labels):
-        all_labels = base_labels.copy()
-        all_labels.update(df_labels)
-        all_labels.update(self.labels)
-
         new_labels = df_labels.copy()
         new_labels.update(self.labels)
+
+        all_labels = base_labels.copy()
+        all_labels.update(new_labels)
 
         applied_alias = False
         not_applied = []
 
-        def add_as_an_alias(new, old, is_old_inherited):
-            self.log.warning("adding label %r as an alias for label %r", new, old)
-            if is_old_inherited and new in all_labels:
-                self.labels[old] = all_labels[new]
-            else:
-                self.labels[new] = all_labels[old]
+        def add_as_an_alias(src, dest):
+            self.log.warning("adding label %r as an alias for label %r", dest, src)
+            self.labels[dest] = all_labels[src]
 
         for old, new in self.aliases.items():
             if old not in all_labels:
@@ -180,18 +176,20 @@ class AddLabelsPlugin(PreBuildPlugin):
             is_old_inherited = old in base_labels and \
                 old not in new_labels
 
+            if is_old_inherited and new in all_labels:
+                add_as_an_alias(new, old)
+
             if new in new_labels:
                 if all_labels[old] != all_labels[new]:
-                    if is_old_inherited:
-                        add_as_an_alias(new, old, is_old_inherited)
-                        continue
                     self.log.warning("labels %r=%r and %r=%r should probably have same value",
                                      old, all_labels[old], new, all_labels[new])
 
                 self.log.debug("alias label %r for %r already exists, skipping", new, old)
                 continue
 
-            add_as_an_alias(new, old, is_old_inherited)
+            if not is_old_inherited or new not in base_labels:
+                add_as_an_alias(old, new)
+
             applied_alias = True
             self.log.info(self.labels)
 
