b21a471e29e6dc81dafc5abd3df266585d9ee20a
Vadim Rutkovsky <vrutkovs@redhat.com>
2017-05-03 16:58:38 +0200

    tito: store version in version.py and generate it on a new release
    Signed-off-by: Vadim Rutkovsky <vrutkovs@redhat.com>

---
+++ atomic_reactor/version.py
@@ -0,0 +1,8 @@
+"""
+Copyright (c) 2017 Red Hat, Inc
+All rights reserved.
+
+This software may be modified and distributed under the terms
+of the BSD license. See the LICENSE file for details.
+"""
+__version__ = "1.6.23"
