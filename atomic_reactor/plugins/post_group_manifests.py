dad62a9224ae8f83aa32a9c1edc07b33005c383b
Mark Langsdorf <mlangsdo@redhat.com>
2017-08-17 14:20:39 -0500

    post_group_manifests: implement creation of the manifest list
    when group=True, group_manifest is meant to create a manifest list.
    It loops through all the platforms in the worker annotations and
    creates a manifest_list_spec, using that platform's worker image
    to create the image entry and the goarch information to create
    the platform information.
    
    The manifest_list_spec is then repeatedly retagged with the tags
    from tag_conf.images, written to a yaml file, and pushed via
    manifest-list. After all the images are tagged, the last image
    is used to create a manifest digest of the manifest list. The manifest
    digest is added to push_conf under the registry's name and the
    image's tag, so that delete_from_registry can delete the
    manifest list later.
    
    A list of the manifest digest from each registry is returned.
    
    Signed-off-by: Mark Langsdorf <mlangsdo@redhat.com>

--- atomic_reactor/plugins/post_group_manifests.py
+++ atomic_reactor/plugins/post_group_manifests.py
@@ -14,11 +14,14 @@ it for all existing image tags.
 from __future__ import unicode_literals
 import requests
 import requests.auth
+from tempfile import NamedTemporaryFile
+import yaml
+from subprocess import check_output, CalledProcessError, STDOUT
 
 from six.moves.urllib.parse import urlparse
 
 from atomic_reactor.plugin import PostBuildPlugin, PluginFailedException
-from atomic_reactor.util import Dockercfg
+from atomic_reactor.util import Dockercfg, get_manifest_digests
 
 
 class GroupManifestsPlugin(PostBuildPlugin):
@@ -46,6 +49,75 @@ class GroupManifestsPlugin(PostBuildPlugin):
         self.goarch = goarch or {}
         self.registries = registries
         self.worker_registries = {}
+
+    def submit_manifest_list(self, registry, registry_conf, manifest_list_spec):
+        docker_secret_path = registry_conf.get('secret', None)
+        with NamedTemporaryFile(prefix='manifest-list', suffix=".yml", mode='w') as fp:
+            yaml.dump(manifest_list_spec, stream=fp)
+            fp.flush()
+            self.log.debug("Wrote to file %s with config %s", fp.name, docker_secret_path)
+
+            # --docker-cfg may be rendundant here, but it's how the tool should work
+            cmd = ['manifest-tool', '--docker-cfg=%s' % docker_secret_path,
+                   'push', 'from-spec', fp.name]
+            # docker always looks in $HOME for the .dockercfg, so set $HOME to the path
+            try:
+                check_output(cmd, stderr=STDOUT, env={'HOME': docker_secret_path})
+            except CalledProcessError as exc:
+                self.log.error("manifest-tool failed with %s", exc.output)
+                raise
+            self.log.info("Manifest list submitted for %s", registry)
+
+    def get_grouped_manifests(self):
+        grouped_manifests = []
+        for registry, registry_conf in self.registries.items():
+            if registry_conf.get('version') == 'v1':
+                continue
+
+            manifest_list_spec = {}
+            manifest_list_spec['manifests'] = []
+            all_annotations = self.workflow.build_result.annotations['worker-builds']
+            for platform in all_annotations:
+                worker_image = all_annotations[platform]['digests'][0]
+                tag = worker_image['tag']
+                repository = worker_image['repository']
+                arch_entry = {
+                    'image': '{0}/{1}:{2}'.format(registry, repository, tag),
+                    'platform': {
+                        'os': 'linux',
+                        'architecture': self.goarch.get(platform, platform)
+                    }
+                }
+                manifest_list_spec['manifests'].append(arch_entry)
+
+            manifest_list_spec['tags'] = [image.tag for image in self.workflow.tag_conf.images]
+            # use a unique image tag because manifest-tool can't accept a digest that
+            # isn't in the respository yet
+            registry_image = self.workflow.tag_conf.unique_images[0]
+            registry_image.registry = registry
+            manifest_list_spec['image'] = registry_image.to_str()
+            self.log.info("Submitting manifest-list spec %s", manifest_list_spec)
+            self.submit_manifest_list(registry, registry_conf, manifest_list_spec)
+            insecure = registry_conf.get('insecure', False)
+            secret_path = registry_conf.get('secret')
+
+            self.log.debug('attempting get_manifest_digests from %s for %s',
+                           registry, registry_image)
+            manifest_list_digest = get_manifest_digests(registry_image, registry=registry,
+                                                        insecure=insecure,
+                                                        dockercfg_path=secret_path,
+                                                        versions=('v2_list',))
+            if not manifest_list_digest.v2_list:
+                raise PluginFailedException('no manifest list digest for %s', registry)
+            self.log.debug('Digest for registry %s is %s', registry, manifest_list_digest.v2_list)
+            push_conf_registry = self.workflow.push_conf.add_docker_registry(registry,
+                                                                             insecure=insecure)
+            tag = registry_image.to_str(registry=False)
+            push_conf_registry.digests[tag] = manifest_list_digest
+            grouped_manifests.append(manifest_list_digest)
+
+        self.log.info("Manifest lists created and collected for all repositories")
+        return grouped_manifests
 
     def get_worker_manifest(self, worker_data):
         worker_digests = worker_data['digests']
@@ -123,10 +195,8 @@ class GroupManifestsPlugin(PostBuildPlugin):
 
     def run(self):
         if self.group:
-            raise NotImplementedError('group=True is not supported in group_manifests')
-        grouped_manifests = []
+            return self.get_grouped_manifests()
 
-        valid = False
         all_annotations = self.workflow.build_result.annotations['worker-builds']
         for plat, annotation in all_annotations.items():
             digests = annotation['digests']
@@ -137,12 +207,8 @@ class GroupManifestsPlugin(PostBuildPlugin):
 
         for platform in all_annotations:
             if self.goarch.get(platform, platform) == 'amd64':
-                valid = True
-                grouped_manifests = self.get_worker_manifest(all_annotations[platform])
-                break
+                self.get_worker_manifest(all_annotations[platform])
+                self.log.debug("found an x86_64 platform and grouped its manifest")
+                return []
 
-        if valid:
-            self.log.debug("found an x86_64 platform and grouped its manifest")
-            return grouped_manifests
-        else:
-            raise ValueError('failed to find an x86_64 platform')
+        raise ValueError('failed to find an x86_64 platform')
