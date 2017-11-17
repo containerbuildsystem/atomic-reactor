9538ab1e4eb6e03c6068b7d02c217030877beb26
Luiz Carvalho <lucarval@redhat.com>
2017-11-17 16:52:32 -0500

    Support tag source and renewing odcs composes
    Signed-off-by: Luiz Carvalho <lucarval@redhat.com>

--- atomic_reactor/odcs_util.py
+++ atomic_reactor/odcs_util.py
@@ -15,11 +15,11 @@ import time
 logger = logging.getLogger(__name__)
 
 
-OIDC_TOKEN_HEADER = 'OIDC_access_token'
-OIDC_CLAIM_SCOPE_HEADER = 'OIDC_CLAIM_scope'
-
-
 class ODCSClient(object):
+
+    OIDC_TOKEN_HEADER = 'OIDC_access_token'
+    OIDC_CLAIM_SCOPE_HEADER = 'OIDC_CLAIM_scope'
+
     def __init__(self, url, insecure=False, token=None):
         if url.endswith('/'):
             self.url = url
@@ -33,48 +33,107 @@ class ODCSClient(object):
     def _auth_headers(self):
         headers = {}
         if self.token:
-            headers[OIDC_TOKEN_HEADER] = self.token
-            headers[OIDC_CLAIM_SCOPE_HEADER] = ('openid https://id.fedoraproject.org/scope/groups '
-                                                'https://pagure.io/odcs/new-compose '
-                                                'https://pagure.io/odcs/renew-compose '
-                                                'https://pagure.io/odcs/delete-compose')
+            headers[self.OIDC_TOKEN_HEADER] = self.token
+            headers[self.OIDC_CLAIM_SCOPE_HEADER] = (
+                'openid https://id.fedoraproject.org/scope/groups '
+                'https://pagure.io/odcs/new-compose '
+                'https://pagure.io/odcs/renew-compose '
+                'https://pagure.io/odcs/delete-compose')
             # FIXME: passing the claim scope in the request doesn't make any sense,
             #   because we're not to be trusted for what scopes we've claimed, but
             #   match what the server wants.
 
         return headers
 
-    def start_compose(self, source_type, source):
+    def start_compose(self, source_type, source, packages=None, sigkeys=None):
+        """Start a new ODCS compose
+
+        :param source_type: str, the type of compose to request (tag, module, pulp)
+        :param source: str, if source_type "tag" is used, the name of the Koji tag
+                       to use when retrieving packages to include in compose;
+                       if source_type "module", white-space separated NAME-STREAM or
+                       NAME-STREAM-VERSION list of modules to include in compose;
+                       if source_type "pulp", white-space separated list of context-sets
+                       to include in compose
+        :param packages: list<str>, packages which should be included in a compose. Only
+                         relevant when source_type "tag" is used.
+        :param sigkeys: list<str>, IDs of signature keys. Only packages signed by one of
+                        these keys will be included in a compose.
+
+        :return: dict, status of compose being created by request.
+        """
         body = {
             'source': {
                 'type': source_type,
                 'source': source
             }
         }
+        if packages:
+            body['source']['packages'] = packages
+
+        if sigkeys is not None:
+            body['source']['sigkeys'] = sigkeys
 
         logger.info("Starting compose for source_type={source_type}, source={source}"
                     .format(source_type=source_type, source=source))
-        response = self.session.post(self.url + 'composes/',
+        response = self.session.post('{}composes/'.format(self.url),
                                      json=body,
                                      headers=self._auth_headers())
         response.raise_for_status()
 
         return response.json()
 
+    def renew_compose(self, compose_id):
+        """Renew, or extend, existing compose
+
+        If the compose has already been removed, ODCS creates a new compose.
+        Otherwise, it extends the time_to_expire of existing compose. In most
+        cases, caller should assume the compose ID will change.
+
+        :param compose_id: int, compose ID to renew
+
+        :return: dict, status of compose being renewed.
+        """
+        logger.info("Renewing compose %d", compose_id)
+        response = self.session.patch('{}composes/{}'.format(self.url, compose_id),
+                                      headers=self._auth_headers())
+        response.raise_for_status()
+        response_json = response.json()
+        compose_id = response_json['id']
+        logger.info("Renewed compose is %d", compose_id)
+        return response_json
+
     def wait_for_compose(self, compose_id,
                          burst_retry=1,
                          burst_length=30,
                          slow_retry=10,
                          timeout=300):
+        """Wait for compose request to finalize
+
+        :param compose_id: int, compose ID to wait for
+        :param burst_retry: int, seconds to wait between retries prior to exceeding
+                            the burst length
+        :param burst_length: int, seconds to switch to slower retry period
+        :param slow_retry: int, seconds to wait between retries after exceeding
+                           the burst length
+        :param timeout: int, when to give up waiting for compose request
+
+        :return: dict, updated status of compose.
+        :raise RuntimeError: if state_name becomes 'failed'
+        """
         logger.debug("Getting compose information for information for compose_id={}"
                      .format(compose_id))
-        url = self.url + 'composes/' + str(compose_id)
+        url = '{}composes/{}'.format(self.url, compose_id)
         headers = self._auth_headers()
         start_time = time.time()
         while True:
             response = self.session.get(url, headers=headers)
             response.raise_for_status()
             response_json = response.json()
+
+            if response_json['state_name'] == 'failed':
+                raise RuntimeError('Failed request for compose_id={}: {!r}'
+                                   .format(compose_id, response_json))
 
             if response_json['state_name'] not in ['wait', 'generating']:
                 logger.debug("Retrieved compose information for compose_id={}: {!r}"
--- tests/test_odcs_util.py
+++ tests/test_odcs_util.py
@@ -6,7 +6,7 @@ This software may be modified and distributed under the terms
 of the BSD license. See the LICENSE file for details.
 """
 
-from atomic_reactor.odcs_util import ODCSClient, OIDC_TOKEN_HEADER
+from atomic_reactor.odcs_util import ODCSClient
 from tests.retry_mock import mock_get_retry_session
 
 import pytest
@@ -20,26 +20,39 @@ MODULE_STREAM = 'f26'
 MODULE_VERSION = "20170629213428"
 MODULE_NSV = '-'.join([MODULE_NAME, MODULE_STREAM, MODULE_VERSION])
 
-ODCS_URL = 'https://odcs.fedoraproject.org/odcs/1'
+ODCS_URL = 'https://odcs.fedoraproject.org/odcs/1/'
 
 COMPOSE_ID = 84
 
+SOURCE_TYPE_ENUM = {
+    'tag': 1,
+    'module': 2,
+}
 
-def compose_json(state, state_name, source_type=2, source=MODULE_NSV):
+
+def compose_json(state, state_name, source_type='module', source=MODULE_NSV,
+                 compose_id=COMPOSE_ID):
     return json.dumps({
         'flags': [],
-        'id': COMPOSE_ID,
+        'id': compose_id,
         'owner': 'Unknown',
         'result_repo': 'http://odcs.fedoraproject.org/composes/latest-odcs-84-1/compose/Temporary',
         'source': source,
-        'source_type': source_type,
+        'source_type': SOURCE_TYPE_ENUM[source_type],
         'state': state,
         'state_name': state_name
     })
 
 
 @responses.activate
-def test_create_compose(tmpdir):
+@pytest.mark.parametrize(('source', 'source_type', 'packages', 'sigkeys'), (
+    (MODULE_NSV, 'module', None, None),
+    ('my-tag', 'tag', ['spam', 'bacon', 'eggs'], None),
+    ('my-tag', 'tag', ['spam', 'bacon', 'eggs'], ['B456', 'R123']),
+    ('my-tag', 'tag', ['spam', 'bacon', 'eggs'], ""),
+    ('my-tag', 'tag', ['spam', 'bacon', 'eggs'], []),
+))
+def test_create_compose(tmpdir, source, source_type, packages, sigkeys):
     odcs_token = 'green_eggs_and_ham'
     secrets_path = tmpdir.mkdir('secret')
     secrets_path.join('token').write(odcs_token)
@@ -47,27 +60,34 @@ def test_create_compose(tmpdir):
     mock_get_retry_session()
 
     def handle_composes_post(request):
-        assert request.headers[OIDC_TOKEN_HEADER] == odcs_token
+        assert request.headers[ODCSClient.OIDC_TOKEN_HEADER] == odcs_token
 
         if isinstance(request.body, six.text_type):
             body = request.body
         else:
             body = request.body.decode()
         body_json = json.loads(body)
-        assert body_json['source']['type'] == 'module'
-        assert body_json['source']['source'] == MODULE_NSV
-        return (200, {}, compose_json(0, 'wait'))
+        assert body_json['source']['type'] == source_type
+        assert body_json['source']['source'] == source
+        assert body_json['source'].get('packages') == packages
+        assert body_json['source'].get('sigkeys') == sigkeys
+        return (200, {}, compose_json(0, 'wait', source_type=source_type, source=source))
 
-    responses.add_callback(responses.POST, ODCS_URL + '/composes/',
+    responses.add_callback(responses.POST, '{}composes/'.format(ODCS_URL),
                            content_type='application/json',
                            callback=handle_composes_post)
 
     odcs_client = ODCSClient(ODCS_URL, token=odcs_token)
-    odcs_client.start_compose(source_type='module', source=MODULE_NSV)
+    odcs_client.start_compose(source_type=source_type, source=source, packages=packages,
+                              sigkeys=sigkeys)
 
 
 @responses.activate
-def test_wait_for_compose(tmpdir):
+@pytest.mark.parametrize(('final_state_id', 'final_state_name', 'expect_exc'), (
+    (2, 'done', False),
+    (4, 'failed', True),
+))
+def test_wait_for_compose(tmpdir, final_state_id, final_state_name, expect_exc):
     odcs_token = 'green_eggs_and_ham'
     secrets_path = tmpdir.mkdir('secret')
     secrets_path.join('token').write(odcs_token)
@@ -82,14 +102,41 @@ def test_wait_for_compose(tmpdir):
         if state['count'] == 1:
             response_json = compose_json(1, 'generating')
         else:
-            response_json = compose_json(2, 'done')
+            response_json = compose_json(final_state_id, final_state_name)
         state['count'] += 1
 
         return (200, {}, response_json)
 
-    responses.add_callback(responses.GET, ODCS_URL + '/composes/%d' % COMPOSE_ID,
+    responses.add_callback(responses.GET, '{}composes/{}'.format(ODCS_URL, COMPOSE_ID),
                            content_type='application/json',
                            callback=handle_composes_get)
 
     odcs_client = ODCSClient(ODCS_URL, token=odcs_token)
-    compose_info = odcs_client.wait_for_compose(COMPOSE_ID)
+    if expect_exc:
+        with pytest.raises(RuntimeError) as exc_info:
+            odcs_client.wait_for_compose(COMPOSE_ID)
+        assert 'Failed request' in str(exc_info.value)
+    else:
+        odcs_client.wait_for_compose(COMPOSE_ID)
+
+
+@responses.activate
+def test_renew_compose(tmpdir):
+    odcs_token = 'green_eggs_and_ham'
+    secrets_path = tmpdir.mkdir('secret')
+    secrets_path.join('token').write(odcs_token)
+
+    mock_get_retry_session()
+
+    new_compose_id = COMPOSE_ID + 1
+
+    def handle_composes_patch(request):
+        assert request.headers[ODCSClient.OIDC_TOKEN_HEADER] == odcs_token
+        return (200, {}, compose_json(0, 'generating', compose_id=new_compose_id))
+
+    responses.add_callback(responses.PATCH, '{}composes/{}'.format(ODCS_URL, COMPOSE_ID),
+                           content_type='application/json',
+                           callback=handle_composes_patch)
+
+    odcs_client = ODCSClient(ODCS_URL, token=odcs_token)
+    odcs_client.renew_compose(COMPOSE_ID)
