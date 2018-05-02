bb9f7b4ebe0b04194c0a5aa4910196df6b49ff19
Luke Meyer <lmeyer@redhat.com>
2018-05-02 21:13:16 +0000

    add build_imagebuilder.py buildstep plugin

---
+++ atomic_reactor/plugins/build_imagebuilder.py
@@ -0,0 +1,83 @@
+"""
+Copyright (c) 2018 Red Hat, Inc
+All rights reserved.
+
+This software may be modified and distributed under the terms
+of the BSD license. See the LICENSE file for details.
+"""
+from __future__ import print_function, unicode_literals
+
+import subprocess
+from time import sleep
+from six import PY2
+
+from atomic_reactor.plugin import BuildStepPlugin
+from atomic_reactor.build import BuildResult
+from atomic_reactor.constants import CONTAINER_IMAGEBUILDER_BUILD_METHOD
+
+
+def sixdecode(data):
+    return data.decode() if PY2 else data
+
+
+class ImagebuilderPlugin(BuildStepPlugin):
+    """
+    Build image using imagebuilder https://github.com/openshift/imagebuilder
+    This requires the imagebuilder executable binary to be in $PATH.
+    """
+
+    key = CONTAINER_IMAGEBUILDER_BUILD_METHOD
+
+    def run(self):
+        """
+        Build image inside current environment using imagebuilder;
+        It's expected this may run within (privileged) docker container.
+
+        Returns:
+            BuildResult
+        """
+        builder = self.workflow.builder
+
+        image = builder.image.to_str()
+        # TODO: directly invoke go imagebuilder library in shared object via python module
+        kwargs = dict(stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if not PY2:
+            kwargs['encoding'] = 'utf-8'
+        ib_process = subprocess.Popen(['imagebuilder', '-t', image, builder.df_dir], **kwargs)
+
+        self.log.debug('imagebuilder build has begun; waiting for it to finish')
+        (output, last_error) = ([], None)
+        while True:
+            poll = ib_process.poll()
+            out = sixdecode(ib_process.stdout.readline())
+            if out:
+                self.log.info(out.strip())
+                output.append(out)
+            err = sixdecode(ib_process.stderr.readline())
+            if err:
+                self.log.error(err.strip())
+                output.append(err)  # include stderr with stdout
+                last_error = err    # while noting the final line
+            if out == '' and err == '':
+                if poll is not None:
+                    break
+                sleep(0.1)  # don't busy-wait when there's no output
+
+        if ib_process.returncode != 0:
+            # imagebuilder uses stderr for normal output too; so in the case of an apparent
+            # failure, single out the last line to include in the failure summary.
+            err = last_error or "<imagebuilder had bad exit code but no error output>"
+            return BuildResult(
+                logs=output,
+                fail_reason="image build failed (rc={}): {}".format(ib_process.returncode, err),
+            )
+
+        # NOTE: it may make more sense to record this in the BuildResult
+        self.workflow.skip_layer_squash = True
+
+        image_id = builder.get_built_image_info()['Id']
+        if ':' not in image_id:
+            # Older versions of the daemon do not include the prefix
+            image_id = 'sha256:{}'.format(image_id)
+
+        return BuildResult(logs=output, image_id=image_id)
