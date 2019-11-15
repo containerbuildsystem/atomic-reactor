ab2b050b19a8c7b3a0ade841585711d120137faf
Tim Waugh <twaugh@redhat.com>
2019-11-15 13:30:04 +0000

    New download_remote_source pre-build plugin
    OSBS-8136
    
    This plugin downloads and unpacks the source code archive from
    Cachito, and sets the appropriate build args.
    
    Signed-off-by: Tim Waugh <twaugh@redhat.com>

---
+++ atomic_reactor/plugins/pre_download_remote_source.py
@@ -0,0 +1,50 @@
+"""
+Copyright (c) 2019 Red Hat, Inc
+All rights reserved.
+
+This software may be modified and distributed under the terms
+of the BSD license. See the LICENSE file for details.
+
+Downloads and unpacks the source code archive from Cachito and sets appropriate build args.
+"""
+
+from __future__ import absolute_import
+
+import tarfile
+
+from atomic_reactor.plugin import PreBuildPlugin
+from atomic_reactor.download import download_url
+
+
+class DownloadRemoteSourcePlugin(PreBuildPlugin):
+    key = 'download_remote_source'
+    is_allowed_to_fail = False
+
+    def __init__(self, tasker, workflow, remote_source_url,
+                 remote_source_build_args=None):
+        """
+        :param tasker: ContainerTasker instance
+        :param workflow: DockerBuildWorkflow instance
+        :param remote_source_url: URL to download source archive from
+        :param remote_source_build_args: dict of container build args
+                                         to be used when building the image
+        """
+        super(DownloadRemoteSourcePlugin, self).__init__(tasker, workflow)
+        self.url = remote_source_url
+        self.buildargs = remote_source_build_args or {}
+
+    def run(self):
+        """
+        Run the plugin.
+        """
+        # Download the source code archive
+        archive = download_url(self.url, self.workflow.source.workdir)
+
+        # Unpack the source code archive into the workdir
+        with tarfile.open(archive) as tf:
+            tf.extractall(self.workflow.source.workdir)
+
+        # Set build args
+        self.workflow.builder.buildargs.update(self.buildargs)
+
+        return archive
---
+++ tests/plugins/test_download_remote_source.py
@@ -0,0 +1,65 @@
+"""
+Copyright (c) 2019 Red Hat, Inc
+All rights reserved.
+
+This software may be modified and distributed under the terms
+of the BSD license. See the LICENSE file for details.
+"""
+
+from __future__ import absolute_import
+
+from io import BytesIO
+import os
+import responses
+import tarfile
+
+from atomic_reactor.inner import DockerBuildWorkflow
+from tests.constants import TEST_IMAGE
+from tests.stubs import StubInsideBuilder
+from atomic_reactor.plugins.pre_download_remote_source import (
+    DownloadRemoteSourcePlugin,
+)
+
+
+class TestDownloadRemoteSource(object):
+    @responses.activate
+    def test_download_remote_source(self, docker_tasker):
+        workflow = DockerBuildWorkflow({"provider": "git", "uri": "asd"},
+                                       TEST_IMAGE)
+        workflow.builder = StubInsideBuilder().for_workflow(workflow)
+        filename = 'source.tar.gz'
+        url = 'https://example.com/dir/{}'.format(filename)
+
+        # Make a compressed tarfile with a single file 'abc'
+        member = 'abc'
+        abc_content = b'def'
+        content = BytesIO()
+        with tarfile.open(mode='w:gz', fileobj=content) as tf:
+            ti = tarfile.TarInfo(name=member)
+            ti.size = len(abc_content)
+            tf.addfile(ti, fileobj=BytesIO(abc_content))
+
+        # GET from the url returns the compressed tarfile
+        responses.add(responses.GET, url, body=content.getvalue())
+
+        buildargs = {'spam': 'maps'}
+        plugin = DownloadRemoteSourcePlugin(docker_tasker, workflow,
+                                            remote_source_url=url,
+                                            remote_source_build_args=buildargs)
+        result = plugin.run()
+
+        # The return value should be the path to the downloaded archive itself
+        with open(result, 'rb') as f:
+            filecontent = f.read()
+
+        assert filecontent == content.getvalue()
+
+        # Expect a file 'abc' in the workdir
+        with open(os.path.join(workflow.source.workdir, member), 'rb') as f:
+            filecontent = f.read()
+
+        assert filecontent == abc_content
+
+        # Expect buildargs to have been set
+        for arg, value in buildargs.items():
+            assert workflow.builder.buildargs[arg] == value
