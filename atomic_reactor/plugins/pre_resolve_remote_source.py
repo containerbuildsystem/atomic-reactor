9938d5d5c9e783232faad1109714fe293fd48f92
Luiz Carvalho <lucarval@redhat.com>
2019-12-11 22:43:22 -0500

    Add the new pre plugin resolve_remote_source
    * OSBS-8135
    
    Signed-off-by: Luiz Carvalho <lucarval@redhat.com>

---
+++ atomic_reactor/plugins/pre_resolve_remote_source.py
@@ -0,0 +1,124 @@
+"""
+Copyright (c) 2019 Red Hat, Inc
+All rights reserved.
+
+This software may be modified and distributed under the terms
+of the BSD license. See the LICENSE file for details.
+"""
+from __future__ import unicode_literals, absolute_import
+
+import os.path
+
+from atomic_reactor.constants import PLUGIN_RESOLVE_REMOTE_SOURCE, REMOTE_SOURCE_DIR
+from atomic_reactor.koji_util import get_koji_task_owner
+from atomic_reactor.plugin import PreBuildPlugin
+from atomic_reactor.plugins.build_orchestrate_build import override_build_kwarg
+from atomic_reactor.plugins.pre_reactor_config import (
+    get_cachito, get_cachito_session, get_koji_session, NO_FALLBACK)
+from atomic_reactor.util import get_build_json
+
+
+class ResolveRemoteSourcePlugin(PreBuildPlugin):
+    """Initiate a new Cachito request for sources
+
+    This plugin will read the remote_sources configuration from
+    container.yaml in the git repository, use it to make a request
+    to Cachito, and wait for the request to complete.
+    """
+
+    key = PLUGIN_RESOLVE_REMOTE_SOURCE
+    is_allowed_to_fail = False
+
+    def __init__(self, tasker, workflow):
+        """
+        :param tasker: ContainerTasker instance
+        :param workflow: DockerBuildWorkflow instance
+        """
+        super(ResolveRemoteSourcePlugin, self).__init__(tasker, workflow)
+        self._cachito_session = None
+        self._osbs = None
+
+    def run(self):
+        try:
+            get_cachito(self.workflow)
+        except KeyError:
+            self.log.info('Aborting plugin execution: missing Cachito configuration')
+            return
+
+        remote_source_config = self.workflow.source.config.remote_source
+        if not remote_source_config:
+            self.log.info('Aborting plugin execution: missing remote_source configuration')
+            return
+
+        user = self.get_koji_user()
+
+        source_request = self.cachito_session.request_sources(user=user, **remote_source_config)
+        source_request = self.cachito_session.wait_for_request(source_request)
+
+        remote_source_json = self.source_request_to_json(source_request)
+        remote_source_url = self.cachito_session.assemble_download_url(source_request)
+        self.set_worker_params(source_request, remote_source_url)
+
+        dest_dir = self.workflow.source.workdir
+        dest_path = self.cachito_session.download_sources(source_request, dest_dir=dest_dir)
+
+        return {
+            # Annotations to be added to the current Build object
+            'annotations': {'remote_source_url': remote_source_url},
+            # JSON representation of the remote source request
+            'remote_source_json': remote_source_json,
+            # Local path to the remote source archive
+            'remote_source_path': dest_path,
+        }
+
+    def set_worker_params(self, source_request, remote_source_url):
+        build_args = {
+            # Turn the environment variables into absolute paths that
+            # represent where the remote sources are copied to during
+            # the build process.
+            env_var: os.path.join(REMOTE_SOURCE_DIR, value)
+            for env_var, value in source_request.get('environment_variables', {}).items()
+        }
+        override_build_kwarg(self.workflow, 'remote_source_url', remote_source_url)
+        override_build_kwarg(self.workflow, 'remote_source_build_args', build_args)
+
+    def source_request_to_json(self, source_request):
+        """Create a relevant representation of the source request"""
+        required = ('ref', 'repo')
+        optional = ('dependencies', 'flags', 'packages', 'pkg_managers', 'environment_variables')
+
+        data = {}
+        try:
+            data.update({k: source_request[k] for k in required})
+        except KeyError:
+            msg = 'Received invalid source request from Cachito: {}'.format(source_request)
+            self.log.exception(msg)
+            raise ValueError(msg)
+
+        data.update({k: source_request.get(k, []) for k in optional})
+
+        return data
+
+    def get_koji_user(self):
+        try:
+            metadata = get_build_json()['metadata']
+        except KeyError:
+            msg = 'Unable to get koji user: No build metadata'
+            self.log.exception('Unable to get koji user: No build metadata')
+            raise ValueError(msg)
+
+        try:
+            koji_task_id = int(metadata.get('labels').get('koji-task-id'))
+        except (ValueError, TypeError, AttributeError):
+            msg = 'Unable to get koji user: Invalid Koji task ID'
+            self.log.exception(msg)
+            raise ValueError(msg)
+
+        koji_session = get_koji_session(self.workflow, NO_FALLBACK)
+        return get_koji_task_owner(koji_session, koji_task_id).get('name')
+
+    @property
+    def cachito_session(self):
+        if not self._cachito_session:
+            self._cachito_session = get_cachito_session(self.workflow)
+        return self._cachito_session
---
+++ tests/plugins/test_resolve_remote_source.py
@@ -0,0 +1,306 @@
+"""
+Copyright (c) 2019 Red Hat, Inc
+All rights reserved.
+
+This software may be modified and distributed under the terms
+of the BSD license. See the LICENSE file for details.
+"""
+
+from __future__ import unicode_literals, absolute_import
+
+from textwrap import dedent
+import sys
+
+from flexmock import flexmock
+import pytest
+
+from atomic_reactor import koji_util
+from atomic_reactor import util
+from atomic_reactor.cachito_util import CachitoAPI
+from atomic_reactor.constants import PLUGIN_BUILD_ORCHESTRATE_KEY
+from atomic_reactor.inner import DockerBuildWorkflow
+from atomic_reactor.plugin import PreBuildPluginsRunner, PluginFailedException
+from atomic_reactor.plugins import pre_reactor_config
+from atomic_reactor.plugins.build_orchestrate_build import (
+    WORKSPACE_KEY_OVERRIDE_KWARGS, OrchestrateBuildPlugin)
+from atomic_reactor.plugins.pre_reactor_config import (
+    ReactorConfigPlugin, WORKSPACE_CONF_KEY, ReactorConfig)
+from atomic_reactor.plugins.pre_resolve_remote_source import ResolveRemoteSourcePlugin
+from atomic_reactor.source import SourceConfig
+
+from tests.constants import TEST_IMAGE
+from tests.stubs import StubInsideBuilder, StubSource
+
+
+KOJI_HUB = 'http://koji.com/hub'
+KOJI_TASK_ID = 123
+KOJI_TASK_OWNER = 'spam'
+
+CACHITO_URL = 'https://cachito.example.com'
+CACHITO_REQUEST_ID = 98765
+CACHITO_REQUEST_DOWNLOAD_URL = '{}/api/v1/{}/download'.format(CACHITO_URL, CACHITO_REQUEST_ID)
+
+REMOTE_SOURCE_REPO = 'https://git.example.com/team/repo.git'
+REMOTE_SOURCE_REF = 'b55c00f45ec3dfee0c766cea3d395d6e21cc2e5a'
+
+
+@pytest.fixture
+def workflow(tmpdir):
+    workflow = DockerBuildWorkflow(
+        TEST_IMAGE,
+        source={"provider": "git", "uri": "asd"}
+    )
+
+    # Stash the tmpdir in workflow so it can be used later
+    workflow._tmpdir = tmpdir
+
+    class MockSource(StubSource):
+
+        def __init__(self, workdir):
+            super(MockSource, self).__init__()
+            self.workdir = workdir
+
+    workflow.source = MockSource(str(tmpdir))
+
+    builder = StubInsideBuilder().for_workflow(workflow)
+    builder.set_df_path(str(tmpdir))
+    builder.tasker = flexmock()
+    workflow.builder = flexmock(builder)
+    workflow.buildstep_plugins_conf = [{'name': PLUGIN_BUILD_ORCHESTRATE_KEY}]
+
+    mock_repo_config(workflow)
+    mock_reactor_config(workflow)
+    mock_build_json()
+    mock_cachito_api(workflow)
+    mock_koji()
+
+    return workflow
+
+
+def mock_reactor_config(workflow, data=None):
+    if data is None:
+        data = dedent("""\
+            version: 1
+            cachito:
+               api_url: {}
+               auth:
+                   ssl_certs_dir: {}
+            """.format(CACHITO_URL, workflow._tmpdir))
+
+    workflow.plugin_workspace[ReactorConfigPlugin.key] = {}
+
+    workflow._tmpdir.join('cert').write('')
+    config = util.read_yaml(data, 'schemas/config.json')
+
+    workflow.plugin_workspace[ReactorConfigPlugin.key][WORKSPACE_CONF_KEY] = ReactorConfig(config)
+
+
+def mock_build_json(build_json=None):
+    if build_json is None:
+        build_json = {'metadata': {'labels': {'koji-task-id': str(KOJI_TASK_ID)}}}
+    flexmock(util).should_receive('get_build_json').and_return(build_json)
+
+
+def mock_repo_config(workflow, data=None):
+    if data is None:
+        data = dedent("""\
+            remote_source:
+                repo: {}
+                ref: {}
+            """.format(REMOTE_SOURCE_REPO, REMOTE_SOURCE_REF))
+
+    workflow._tmpdir.join('container.yaml').write(data)
+
+    # The repo config is read when SourceConfig is initialized. Force
+    # reloading here to make usage easier.
+    workflow.source.config = SourceConfig(str(workflow._tmpdir))
+
+
+def mock_cachito_api(workflow, user=KOJI_TASK_OWNER, source_request=None):
+    if source_request is None:
+        source_request = {
+            'id': CACHITO_REQUEST_ID,
+            'repo': REMOTE_SOURCE_REPO,
+            'ref': REMOTE_SOURCE_REF,
+            'environment_variables': {
+                'GOPATH': 'deps/gomod',
+                'GOCACHE': 'deps/gomod',
+            },
+            'flags': ['enable-confeti', 'enable-party-popper'],
+            'pkg_managers': ['gomod'],
+            'dependencies': [
+                {
+                    'name': 'github.com/op/go-logging',
+                    'type': 'gomod',
+                    'version': 'v0.1.1',
+                }
+            ],
+            'packages': [
+                {
+                    'name': 'github.com/spam/bacon/v2',
+                    'type': 'gomod',
+                    'version': 'v2.0.3'
+                }
+            ],
+            'extra_cruft': 'ignored',
+        }
+
+    (flexmock(CachitoAPI)
+        .should_receive('request_sources')
+        .with_args(
+            repo=REMOTE_SOURCE_REPO,
+            ref=REMOTE_SOURCE_REF,
+            user=user,
+        )
+        .and_return({'id': CACHITO_REQUEST_ID}))
+
+    (flexmock(CachitoAPI)
+        .should_receive('wait_for_request')
+        .with_args({'id': CACHITO_REQUEST_ID})
+        .and_return(source_request))
+    (flexmock(CachitoAPI)
+        .should_receive('download_sources')
+        .with_args(source_request, dest_dir=str(workflow._tmpdir))
+        .and_return(expected_dowload_path(workflow)))
+
+    (flexmock(CachitoAPI)
+        .should_receive('assemble_download_url')
+        .with_args(source_request)
+        .and_return(CACHITO_REQUEST_DOWNLOAD_URL))
+
+
+def mock_koji(user=KOJI_TASK_OWNER):
+    koji_session = flexmock()
+    flexmock(pre_reactor_config).should_receive('get_koji_session').and_return(koji_session)
+    flexmock(koji_util).should_receive('get_koji_task_owner').and_return({'name': user})
+
+
+def expected_dowload_path(workflow):
+    return workflow._tmpdir.join('source.tar.gz')
+
+
+def setup_function(*args):
+    # IMPORTANT: This needs to be done to ensure mocks at the module
+    # level are reset between test cases.
+    sys.modules.pop('pre_resolve_remote_source', None)
+
+
+def teardown_function(*args):
+    # IMPORTANT: This needs to be done to ensure mocks at the module
+    # level are reset between test cases.
+    sys.modules.pop('pre_resolve_remote_source', None)
+
+
+def test_resolve_remote_source(workflow):
+    run_plugin_with_args(workflow)
+
+
+@pytest.mark.parametrize('pop_key', ('repo', 'ref'))
+def test_invalid_remote_source_structure(workflow, pop_key):
+    source_request = {
+        'id': CACHITO_REQUEST_ID,
+        'repo': REMOTE_SOURCE_REPO,
+        'ref': REMOTE_SOURCE_REF,
+    }
+    source_request.pop(pop_key)
+    mock_cachito_api(workflow, source_request=source_request)
+    run_plugin_with_args(workflow, expect_error='Received invalid source request')
+
+
+def test_ignore_when_missing_cachito_config(workflow):
+    reactor_config = dedent("""\
+        version: 1
+        """)
+    mock_reactor_config(workflow, reactor_config)
+    result = run_plugin_with_args(workflow, expect_result=False)
+    assert result is None
+
+
+def test_invalid_cert_reference(workflow):
+    bad_certs_dir = str(workflow._tmpdir.join('invalid-dir'))
+    reactor_config = dedent("""\
+        version: 1
+        cachito:
+           api_url: {}
+           auth:
+               ssl_certs_dir: {}
+        """.format(CACHITO_URL, bad_certs_dir))
+    mock_reactor_config(workflow, reactor_config)
+    run_plugin_with_args(workflow, expect_error="Cachito ssl_certs_dir doesn't exist")
+
+
+def test_ignore_when_missing_remote_source_config(workflow):
+    remote_source_config = dedent("""---""")
+    mock_repo_config(workflow, remote_source_config)
+    result = run_plugin_with_args(workflow, expect_result=False)
+    assert result is None
+
+
+@pytest.mark.parametrize(('build_json', 'expect_error'), (
+    ({}, 'No build metadata'),
+    ({'metadata': None}, 'Invalid Koji task ID'),
+    ({'metadata': {}}, 'Invalid Koji task ID'),
+    ({'metadata': {'labels': {}}}, 'Invalid Koji task ID'),
+    ({'metadata': {'labels': {'koji-task-id': None}}}, 'Invalid Koji task ID'),
+    ({'metadata': {'labels': {'koji-task-id': 'not-an-int'}}}, 'Invalid Koji task ID'),
+))
+def test_bad_build_metadata(workflow, build_json, expect_error):
+    mock_build_json(build_json=build_json)
+    run_plugin_with_args(workflow, expect_error=expect_error)
+
+
+def run_plugin_with_args(workflow, expect_error=None, expect_result=True):
+    runner = PreBuildPluginsRunner(
+        workflow.builder.tasker,
+        workflow,
+        [
+            {'name': ResolveRemoteSourcePlugin.key, 'args': {}},
+        ]
+    )
+
+    if expect_error:
+        with pytest.raises(PluginFailedException, match=expect_error):
+            runner.run()
+        return
+
+    results = runner.run()[ResolveRemoteSourcePlugin.key]
+
+    if expect_result:
+        assert results['annotations']['remote_source_url']
+        assert results['remote_source_json'] == {
+            'repo': REMOTE_SOURCE_REPO,
+            'ref': REMOTE_SOURCE_REF,
+            'environment_variables': {
+                'GOPATH': 'deps/gomod',
+                'GOCACHE': 'deps/gomod',
+            },
+            'flags': ['enable-confeti', 'enable-party-popper'],
+            'pkg_managers': ['gomod'],
+            'dependencies': [
+                {
+                    'name': 'github.com/op/go-logging',
+                    'type': 'gomod',
+                    'version': 'v0.1.1',
+                }
+            ],
+            'packages': [
+                {
+                    'name': 'github.com/spam/bacon/v2',
+                    'type': 'gomod',
+                    'version': 'v2.0.3'
+                }
+            ],
+        }
+        assert results['remote_source_path'] == expected_dowload_path(workflow)
+
+        # A result means the plugin was enabled and executed successfully.
+        # Let's verify the expected side effects.
+        orchestrator_build_workspace = workflow.plugin_workspace[OrchestrateBuildPlugin.key]
+        worker_params = orchestrator_build_workspace[WORKSPACE_KEY_OVERRIDE_KWARGS][None]
+        assert worker_params['remote_source_url'] == CACHITO_REQUEST_DOWNLOAD_URL
+        assert worker_params['remote_source_build_args'] == {
+            'GOPATH': '/remote-source/deps/gomod',
+            'GOCACHE': '/remote-source/deps/gomod',
+        }
+
+    return results
