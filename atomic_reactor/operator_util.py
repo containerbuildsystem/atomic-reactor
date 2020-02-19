ecb8764432e3a1f6b34f336cabd86cc107f71e1c
Adam Cmiel <acmiel@redhat.com>
2020-02-19 17:01:20 +0100

    operator_util: Implement OperatorCSV
    * OSBS-8636
    
    OperatorCSV represents a single ClusterServiceVersion file in an
    operator manifest. It provides methods for finding and replacing
    pullspecs in predefined locations.
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

---
+++ atomic_reactor/operator_util.py
@@ -0,0 +1,175 @@
+"""
+Copyright (c) 2020 Red Hat, Inc
+All rights reserved.
+
+This software may be modified and distributed under the terms
+of the BSD license. See the LICENSE file for details.
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+import logging
+
+from ruamel.yaml import YAML
+from ruamel.yaml.comments import CommentedMap, CommentedSeq
+
+from atomic_reactor.util import ImageName, chain_get
+
+
+yaml = YAML()
+log = logging.getLogger(__name__)
+
+
+class OperatorCSV(object):
+    """
+    A single ClusterServiceVersion file in an operator manifest.
+
+    Can find and replace pullspecs for container images in predefined locations.
+    """
+
+    def __init__(self, path, data):
+        self.path = path
+        self.data = data
+
+    @classmethod
+    def from_file(cls, path):
+        """
+        Make an OperatorCSV from a file
+
+        :param path: Path to file
+        :return: OperatorCSV
+        """
+        with open(path) as f:
+            data = yaml.load(f)
+            return cls(path, data)
+
+    def dump(self):
+        """
+        Write data to file (preserves comments)
+        """
+        with open(self.path, "w") as f:
+            yaml.dump(self.data, f)
+
+    def get_pullspecs(self):
+        """
+        Find pullspecs for containers, related images and RELATED_IMAGE_*
+        environment variables in containers.
+
+        :return: set of ImageName pullspecs
+        """
+        pullspecs = self._get_related_image_pullspecs()
+        pullspecs.update(self._get_env_pullspecs())
+        pullspecs.update(self._get_container_image_pullspecs())
+        return pullspecs
+
+    def replace_pullspecs(self, replacement_pullspecs):
+        """
+        Replace pullspecs for containers, related images and RELATED_IMAGE_*
+        environment variables in containers.
+
+        :param replacement_pullspecs: mapping of pullspec -> replacement
+        """
+        self._replace_related_image_pullspecs(replacement_pullspecs)
+        self._replace_env_pullspecs(replacement_pullspecs)
+        self._replace_container_image_pullspecs(replacement_pullspecs)
+
+    def replace_pullspecs_everywhere(self, replacement_pullspecs):
+        """
+        Replace all pullspecs found anywhere in data
+
+        :param replacement_pullspecs: mapping of pullspec -> replacment
+        """
+        log_template = "%(path)s - Replaced pullspec: %(old)s -> %(new)s"
+        for k in self.data:
+            self._replace_pullspecs_everywhere(self.data, k, replacement_pullspecs, log_template)
+
+    def _get_related_images(self):
+        related_images_path = ("spec", "relatedImages")
+        return chain_get(self.data, related_images_path, default=[])
+
+    def _get_containers(self):
+        deployments_path = ("spec", "install", "spec", "deployments")
+        deployments = chain_get(self.data, deployments_path, default=[])
+
+        containers_path = ("spec", "template", "spec", "containers")
+        containers = [
+            c for d in deployments for c in chain_get(d, containers_path, default=[])
+        ]
+        return containers
+
+    def _get_related_image_envs(self):
+        containers = self._get_containers()
+        envs = [
+            e for c in containers
+            for e in c.get("env", []) if e["name"].startswith("RELATED_IMAGE_")
+        ]
+        for env in envs:
+            if "valueFrom" in env:
+                msg = '{}: "valueFrom" references are not supported'.format(env["name"])
+                raise RuntimeError(msg)
+        return envs
+
+    def _get_pullspec(self, obj, key, log_template=None):
+        pullspec = ImageName.parse(obj[key])
+        if log_template is not None:
+            name = obj.get("name") if isinstance(obj, CommentedMap) else None
+            log.debug(log_template,
+                      {"path": self.path, "name": name, "pullspec": pullspec})
+        return pullspec
+
+    def _get_related_image_pullspecs(self):
+        rel_images = self._get_related_images()
+        log_template = "%(path)s - Found pullspec for related image %(name)s: %(pullspec)s"
+        return set(self._get_pullspec(i, "image", log_template) for i in rel_images)
+
+    def _get_env_pullspecs(self):
+        envs = self._get_related_image_envs()
+        log_template = "%(path)s - Found pullspec in %(name)s var: %(pullspec)s"
+        return set(self._get_pullspec(e, "value", log_template) for e in envs)
+
+    def _get_container_image_pullspecs(self):
+        containers = self._get_containers()
+        log_template = "%(path)s - Found pullspec for container %(name)s: %(pullspec)s"
+        return set(self._get_pullspec(c, "image", log_template) for c in containers)
+
+    def _replace_pullspec(self, obj, key, replacement_pullspecs, log_template=None):
+        old = ImageName.parse(obj[key])
+        new = replacement_pullspecs.get(old)
+        if new is None or new == old:
+            return
+        if log_template is not None:
+            name = obj.get("name") if isinstance(obj, CommentedMap) else None
+            log.debug(log_template,
+                      {"path": self.path, "name": name, "old": old, "new": new})
+        obj[key] = str(new)  # `new` is an ImageName
+
+    def _replace_related_image_pullspecs(self, replacement_pullspecs):
+        related_images = self._get_related_images()
+        log_tmpl = "%(path)s - Replaced pullspec for related image %(name)s: %(old)s -> %(new)s"
+        for i in related_images:
+            self._replace_pullspec(i, "image", replacement_pullspecs, log_tmpl)
+
+    def _replace_env_pullspecs(self, replacement_pullspecs):
+        envs = self._get_related_image_envs()
+        log_template = "%(path)s - Replaced pullspec in %(name)s var: %(old)s -> %(new)s"
+        for e in envs:
+            self._replace_pullspec(e, "value", replacement_pullspecs, log_template)
+
+    def _replace_container_image_pullspecs(self, replacement_pullspecs):
+        containers = self._get_containers()
+        log_template = "%(path)s - Replaced pullspec for container %(name)s: %(old)s -> %(new)s"
+        for c in containers:
+            self._replace_pullspec(c, "image", replacement_pullspecs, log_template)
+
+    def _replace_pullspecs_everywhere(self, obj, k_or_i, replacement_pullspecs, log_template):
+        item = obj[k_or_i]
+        if isinstance(item, CommentedMap):
+            for k in item:
+                self._replace_pullspecs_everywhere(item, k, replacement_pullspecs, log_template)
+        elif isinstance(item, CommentedSeq):
+            for i in range(len(item)):
+                self._replace_pullspecs_everywhere(item, i, replacement_pullspecs, log_template)
+        elif isinstance(item, str):
+            # Doesn't matter if string was not a pullspec, it will simply not match anything
+            # in replacement_pullspecs and no replacement will be done
+            self._replace_pullspec(obj, k_or_i, replacement_pullspecs, log_template)
---
+++ tests/test_operator_util.py
@@ -0,0 +1,371 @@
+"""
+Copyright (c) 2020 Red Hat, Inc
+All rights reserved.
+
+This software may be modified and distributed under the terms
+of the BSD license. See the LICENSE file for details.
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+import copy
+
+import pytest
+from ruamel.yaml import YAML
+
+from atomic_reactor.util import ImageName, chain_get
+from atomic_reactor.operator_util import OperatorCSV
+
+
+yaml = YAML()
+
+
+class PullSpec(object):
+    def __init__(self, name, value, replace, path):
+        self._name = name
+        self._value = ImageName.parse(value)
+        self._replace = ImageName.parse(replace)
+        self._path = path
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def value(self):
+        return self._value
+
+    @property
+    def replace(self):
+        return self._replace
+
+    @property
+    def path(self):
+        return tuple(self._path)
+
+    def __str__(self):
+        return str(self.value)
+
+    def find_in_data(self, data):
+        return ImageName.parse(chain_get(data, self.path))
+
+
+FOO = PullSpec(
+    "foo", "foo:1", "r-foo:2",
+    ["spec", "relatedImages", 0, "image"]
+)
+BAR = PullSpec(
+    "bar", "registry/bar:1", "r-registry/r-bar:2",
+    ["spec", "relatedImages", 1, "image"]
+)
+SPAM = PullSpec(
+    "spam", "registry/namespace/spam:1", "r-registry/r-namespace/r-spam:2",
+    ["spec", "install", "spec", "deployments", 0,
+     "spec", "template", "spec", "containers", 0, "image"]
+)
+EGGS = PullSpec(
+    "eggs", "eggs:1", "r-eggs:2",
+    ["spec", "install", "spec", "deployments", 0,
+     "spec", "template", "spec", "containers", 0, "env", 0, "value"]
+)
+HAM = PullSpec(
+    "ham", "ham:1", "r-ham:2",
+    ["spec", "install", "spec", "deployments", 0,
+     "spec", "template", "spec", "containers", 1, "image"]
+)
+JAM = PullSpec(
+    "jam", "jam:1", "r-jam:2",
+    ["spec", "install", "spec", "deployments", 1,
+     "spec", "template", "spec", "containers", 0, "image"]
+)
+
+PULLSPECS = {p.name: p for p in [FOO, BAR, SPAM, EGGS, HAM, JAM]}
+
+
+ORIGINAL_CONTENT = """\
+# A meaningful comment
+spec:
+  relatedImages:
+  - name: foo
+    image: {foo}
+  - name: bar
+    image: {bar}
+  install:
+    spec:
+      deployments:
+      - spec:
+          template:
+            spec:
+              containers:
+              - name: spam
+                image: {spam}
+                env:
+                - name: RELATED_IMAGE_EGGS
+                  value: {eggs}
+                - name: UNRELATED_IMAGE
+                  value: {eggs}
+              - name: ham
+                image: {ham}
+      - spec:
+          template:
+            spec:
+              containers:
+              - name: jam
+                image: {jam}
+random:
+  nested:
+    dict:
+      a: {foo}
+      b: {bar}
+      c: {spam}
+      d: {eggs}
+      e: {ham}
+      f: {jam}
+    list:
+    - {foo}
+    - {bar}
+    - {spam}
+    - {eggs}
+    - {ham}
+    - {jam}
+""".format(**PULLSPECS)
+
+REPLACED_CONTENT = """\
+# A meaningful comment
+spec:
+  relatedImages:
+  - name: foo
+    image: {foo.replace}
+  - name: bar
+    image: {bar.replace}
+  install:
+    spec:
+      deployments:
+      - spec:
+          template:
+            spec:
+              containers:
+              - name: spam
+                image: {spam.replace}
+                env:
+                - name: RELATED_IMAGE_EGGS
+                  value: {eggs.replace}
+                - name: UNRELATED_IMAGE
+                  value: {eggs}
+              - name: ham
+                image: {ham.replace}
+      - spec:
+          template:
+            spec:
+              containers:
+              - name: jam
+                image: {jam.replace}
+random:
+  nested:
+    dict:
+      a: {foo}
+      b: {bar}
+      c: {spam}
+      d: {eggs}
+      e: {ham}
+      f: {jam}
+    list:
+    - {foo}
+    - {bar}
+    - {spam}
+    - {eggs}
+    - {ham}
+    - {jam}
+""".format(**PULLSPECS)
+
+REPLACED_EVERYWHERE_CONTENT = """\
+# A meaningful comment
+spec:
+  relatedImages:
+  - name: foo
+    image: {foo.replace}
+  - name: bar
+    image: {bar.replace}
+  install:
+    spec:
+      deployments:
+      - spec:
+          template:
+            spec:
+              containers:
+              - name: spam
+                image: {spam.replace}
+                env:
+                - name: RELATED_IMAGE_EGGS
+                  value: {eggs.replace}
+                - name: UNRELATED_IMAGE
+                  value: {eggs.replace}
+              - name: ham
+                image: {ham.replace}
+      - spec:
+          template:
+            spec:
+              containers:
+              - name: jam
+                image: {jam.replace}
+random:
+  nested:
+    dict:
+      a: {foo.replace}
+      b: {bar.replace}
+      c: {spam.replace}
+      d: {eggs.replace}
+      e: {ham.replace}
+      f: {jam.replace}
+    list:
+    - {foo.replace}
+    - {bar.replace}
+    - {spam.replace}
+    - {eggs.replace}
+    - {ham.replace}
+    - {jam.replace}
+""".format(**PULLSPECS)
+
+
+class CSVFile(object):
+    def __init__(self, content):
+        self.content = content
+        self._data = yaml.load(content)
+
+    @property
+    def data(self):
+        return copy.deepcopy(self._data)
+
+
+ORIGINAL = CSVFile(ORIGINAL_CONTENT)
+REPLACED = CSVFile(REPLACED_CONTENT)
+REPLACED_EVERYWHERE = CSVFile(REPLACED_EVERYWHERE_CONTENT)
+
+
+class TestOperatorCSV(object):
+    _original_pullspecs = {p.value for p in PULLSPECS.values()}
+    _replacement_pullspecs = {p.value: p.replace for p in PULLSPECS.values()}
+
+    def test_from_file(self, tmpdir):
+        path = tmpdir.join("original.yaml")
+        path.write(ORIGINAL.content)
+
+        csv = OperatorCSV.from_file(str(path))
+        assert csv.path == str(path)
+        assert csv.data == ORIGINAL.data
+
+    def test_get_pullspecs(self, caplog):
+        csv = OperatorCSV("original.yaml", ORIGINAL.data)
+        pullspecs = csv.get_pullspecs()
+        assert pullspecs == self._original_pullspecs
+
+        expected_logs = [
+            "original.yaml - Found pullspec for related image foo: {foo}",
+            "original.yaml - Found pullspec for related image bar: {bar}",
+            "original.yaml - Found pullspec in RELATED_IMAGE_EGGS var: {eggs}",
+            "original.yaml - Found pullspec for container spam: {spam}",
+            "original.yaml - Found pullspec for container ham: {ham}",
+            "original.yaml - Found pullspec for container jam: {jam}"
+        ]
+        for log in expected_logs:
+            assert log.format(**PULLSPECS) in caplog.text
+
+    def test_replace_pullspecs(self, caplog):
+        csv = OperatorCSV("original.yaml", ORIGINAL.data)
+        csv.replace_pullspecs(self._replacement_pullspecs)
+        assert csv.data == REPLACED.data
+
+        expected_logs = [
+            "{file} - Replaced pullspec for related image foo: {foo} -> {foo.replace}",
+            "{file} - Replaced pullspec for related image bar: {bar} -> {bar.replace}",
+            "{file} - Replaced pullspec in RELATED_IMAGE_EGGS var: {eggs} -> {eggs.replace}",
+            "{file} - Replaced pullspec for container spam: {spam} -> {spam.replace}",
+            "{file} - Replaced pullspec for container ham: {ham} -> {ham.replace}",
+            "{file} - Replaced pullspec for container jam: {jam} -> {jam.replace}"
+        ]
+        for log in expected_logs:
+            assert log.format(file="original.yaml", **PULLSPECS) in caplog.text
+
+    def test_replace_pullspecs_everywhere(self, caplog):
+        csv = OperatorCSV("original.yaml", ORIGINAL.data)
+        csv.replace_pullspecs_everywhere(self._replacement_pullspecs)
+        assert csv.data == REPLACED_EVERYWHERE.data
+
+        expected_logs = {
+            "original.yaml - Replaced pullspec: {foo} -> {foo.replace}": 3,
+            "original.yaml - Replaced pullspec: {bar} -> {bar.replace}": 3,
+            "original.yaml - Replaced pullspec: {eggs} -> {eggs.replace}": 4,
+            "original.yaml - Replaced pullspec: {spam} -> {spam.replace}": 3,
+            "original.yaml - Replaced pullspec: {ham} -> {ham.replace}": 3,
+            "original.yaml - Replaced pullspec: {jam} -> {jam.replace}": 3
+        }
+        for log, count in expected_logs.items():
+            assert caplog.text.count(log.format(**PULLSPECS)) == count
+
+    def test_dump(self, tmpdir):
+        path = tmpdir.join("original.yaml")
+        csv = OperatorCSV(str(path), ORIGINAL.data)
+        csv.dump()
+
+        content = path.read()
+        # Formatting does not necessarily have to match, at least check the data...
+        assert yaml.load(content) == csv.data
+        # ...and that the comment was preserved
+        assert content.startswith('# A meaningful comment')
+
+    def test_replace_only_some_pullspecs(self, caplog):
+        replacement_pullspecs = self._replacement_pullspecs.copy()
+
+        # Foo won't be replaced because replacement is identical
+        replacement_pullspecs[FOO.value] = FOO.value
+        # Bar won't be replaced because no replacement available
+        del replacement_pullspecs[BAR.value]
+
+        csv = OperatorCSV("original.yaml", ORIGINAL.data)
+        csv.replace_pullspecs(replacement_pullspecs)
+
+        assert FOO.find_in_data(csv.data) == FOO.value
+        assert BAR.find_in_data(csv.data) == BAR.value
+
+        foo_log = "original.yaml - Replaced pullspec for related image foo: {foo}"
+        bar_log = "original.yaml - Replaced pullspec for related image bar: {bar}"
+
+        assert foo_log.format(foo=FOO) not in caplog.text
+        assert bar_log.format(bar=BAR) not in caplog.text
+
+    @pytest.mark.parametrize("rel_images, rel_envs, containers, expected", [
+        (False, False, False, set()),
+        (True, False, False, {FOO.value, BAR.value}),
+        # (False, True, False) - Cannot have envs without containers
+        (False, False, True, {SPAM.value, HAM.value, JAM.value}),
+        # (True, True, False) - Cannot have envs without containers
+        (True, False, True, {FOO.value, BAR.value, SPAM.value, HAM.value, JAM.value}),
+        (False, True, True, {SPAM.value, EGGS.value, HAM.value, JAM.value}),
+    ])
+    def test_get_pullspecs_some_locations(self, rel_images, rel_envs, containers, expected):
+        data = ORIGINAL.data
+        if not rel_images:
+            del data["spec"]["relatedImages"]
+        deployments = chain_get(data, ["spec", "install", "spec", "deployments"])
+        if not rel_envs:
+            for d in deployments:
+                for c in chain_get(d, ["spec", "template", "spec", "containers"]):
+                    c.pop("env", None)
+        if not containers:
+            for d in deployments:
+                del d["spec"]["template"]["spec"]["containers"]
+
+        csv = OperatorCSV("x.yaml", data)
+        assert csv.get_pullspecs() == expected
+
+    def test_valuefrom_references_not_allowed(self):
+        data = ORIGINAL.data
+        env_path = EGGS.path[:-1]
+        env = chain_get(data, env_path)
+        env["valueFrom"] = "somewhere"
+
+        csv = OperatorCSV("original.yaml", data)
+        with pytest.raises(RuntimeError) as exc_info:
+            csv.get_pullspecs()
+
+        assert '"valueFrom" references are not supported' in str(exc_info.value)
