ff6fb9dc115870661a6b925eff2a59f16de8bb66
Martin Bašti <mbasti@redhat.com>
2020-03-03 16:07:58 +0100

    Fix: don't run dockerfile checks for Flatpak
    Flatpak builds have no user Dockerfiles specified
    
    * OSBS-8634
    
    Signed-off-by: Martin Bašti <mbasti@redhat.com>

--- atomic_reactor/plugins/pre_check_user_settings.py
+++ atomic_reactor/plugins/pre_check_user_settings.py
@@ -30,8 +30,24 @@ class CheckUserSettingsPlugin(PreBuildPlugin):
     key = PLUGIN_CHECK_USER_SETTINGS
     is_allowed_to_fail = False
 
+    def __init__(self, tasker, workflow, flatpak=False):
+        """
+        :param tasker: ContainerTasker instance
+        :param workflow: DockerBuildWorkflow instance
+        :param flatpak: bool, if build is for flatpak
+        """
+        super(CheckUserSettingsPlugin, self).__init__(tasker, workflow)
+
+        self.flatpak = flatpak
+
     def dockerfile_checks(self):
         """Checks for Dockerfile"""
+        if self.flatpak:
+            self.log.info(
+                "Skipping Dockerfile checks because this is flatpak build "
+                "without user Dockerfile")
+            return
+
         self.appregistry_bundle_label_mutually_exclusive()
 
     def appregistry_bundle_label_mutually_exclusive(self):
