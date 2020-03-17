c7b4fff02462e5cbac826697dd50fdfcc04465a6
Adam Cmiel <acmiel@redhat.com>
2020-03-17 14:29:37 +0100

    Fix __future__ import in pin_operator_digest
    It was not supposed to be print_function but unicode_literals
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- atomic_reactor/plugins/pre_pin_operator_digest.py
+++ atomic_reactor/plugins/pre_pin_operator_digest.py
@@ -6,7 +6,7 @@ This software may be modified and distributed under the terms
 of the BSD license. See the LICENSE file for details.
 """
 
-from __future__ import absolute_import, print_function
+from __future__ import absolute_import, unicode_literals
 
 import os.path
 
