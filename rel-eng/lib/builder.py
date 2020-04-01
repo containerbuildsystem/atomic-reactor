90155176e6267a123afa8279fe2c037c89056635
Martin Styk <mastyk@redhat.com>
2020-04-01 11:47:55 +0200

    feat: add support for tito custom builder
    Signed-off-by: Martin Styk <mastyk@redhat.com>

---
+++ rel-eng/lib/builder.py
@@ -0,0 +1,12 @@
+from tito.builder import Builder
+
+
+class AtomicReactorBuilder(Builder):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # tarball has to represent Source0
+        # but internal structure should remain same
+        # i.e. {name}-{version} otherwise %setup -q
+        # will fail
+        self.tgz_filename = self.display_version + ".tar.gz"
