containerbuildsystem/atomic-reactor

commit 917335db4b97682cb8dddf6cd7fd58a2a1343c6e

Set proper image_id for flatpak builds
The builder image_id is used to set extra.docker.id in the koji archive
metadata. For regular builds, the image id is the built image config
digest. For flatpak builds, this is set as the image config id for the
file system built by the docker_api builder. This is not the final image
config digest since flatpak will build a new OCI image using the
aforementioned file system. This patch sets the image_id to the OCI
image config built by flatpak.

The actual image_id should not change in the case the image is converted
from OCI to docker upon a registry push (given the JSON file order is
kept). The OCI image config and docker image config specs are
compatible.

* OSBS-8780

Signed-off-by: Athos Ribeiro <athos@redhat.com>

atomic_reactor/plugins/prepub_flatpak_create_oci.py
@@ -12,6 +12,9 @@ and turns it into a Flatpak application or runtime.
 
 from __future__ import absolute_import
 
+import json
+import subprocess
+
 from flatpak_module_tools.flatpak_builder import FlatpakBuilder, FLATPAK_METADATA_ANNOTATIONS
 
 from atomic_reactor.constants import IMAGE_TYPE_OCI, IMAGE_TYPE_OCI_TAR
@@ -93,6 +96,12 @@ class FlatpakCreateOciPlugin(PrePublishPlugin):
             self.log.info("Cleaning up docker container")
             self.tasker.remove_container(container_id)
 
+    def _get_oci_image_id(self, oci_path):
+        cmd = ['skopeo', 'inspect', '--raw', 'oci:{}'.format(oci_path)]
+        raw_manifest = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+        oci_image_manifest = json.loads(raw_manifest)
+        return oci_image_manifest['config']['digest']
+
     def run(self):
         source = get_flatpak_source_info(self.workflow)
         if source is None:
@@ -114,6 +123,10 @@ class FlatpakCreateOciPlugin(PrePublishPlugin):
         self.workflow.image_components = image_components
 
         ref_name, outfile, tarred_outfile = self.builder.build_container(tarred_filesystem)
+
+        image_id = self._get_oci_image_id(outfile)
+        self.log.info('New OCI image ID is %s', image_id)
+        self.workflow.builder.image_id = image_id
 
         metadata = get_exported_image_metadata(outfile, IMAGE_TYPE_OCI)
         metadata['ref_name'] = ref_name

tests/plugins/test_flatpak_create_oci.py
@@ -363,10 +363,11 @@ class MockSource(object):
     path = None
 
 
-class X(object):
-    image_id = "xxx"
-    source = MockSource()
-    base_image = ImageName(repo="qwe", tag="asd")
+class MockBuilder(object):
+    def __init__(self):
+        self.image_id = "xxx"
+        self.source = MockSource()
+        self.base_image = ImageName(repo="qwe", tag="asd")
 
 
 def load_labels_and_annotations(metadata):
@@ -491,14 +492,20 @@ def test_flatpak_create_oci(tmpdir, docker_tasker, config_name, flatpak_metadata
     if not have_flatpak:
         return
 
+    # Check if we have skopeo
+    try:
+        subprocess.check_output(['skopeo', '--version'])
+    except (subprocess.CalledProcessError, OSError):
+        pytest.skip(msg='skopeo not available')
+
     config = CONFIGS[config_name]
 
     workflow = DockerBuildWorkflow(
         TEST_IMAGE,
         source={"provider": "git", "uri": "asd"}
     )
-    setattr(workflow, 'builder', X)
-    X.df_path = write_docker_file(config, str(tmpdir))
+    setattr(workflow, 'builder', MockBuilder())
+    workflow.builder.df_path = write_docker_file(config, str(tmpdir))
     setattr(workflow.builder, 'tasker', docker_tasker)
 
     make_and_store_reactor_config_map(workflow, flatpak_metadata)
@@ -595,7 +602,9 @@ def test_flatpak_create_oci(tmpdir, docker_tasker, config_name, flatpak_metadata
             runner.run()
         assert expected_exception in str(ex.value)
     else:
+        assert workflow.builder.image_id == 'xxx'
         runner.run()
+        assert re.match(r'^sha256:\w{64}$', workflow.builder.image_id)
 
         dir_metadata = workflow.exported_image_sequence[-2]
         assert dir_metadata['type'] == IMAGE_TYPE_OCI

test.sh
@@ -33,7 +33,7 @@ if [[ $OS == "fedora" ]]; then
   PIP="pip$PYTHON_VERSION"
   PKG="dnf"
   ENABLE_REPO="--enablerepo=updates-testing"
-  PKG_EXTRA="dnf-plugins-core desktop-file-utils flatpak ostree python$PYTHON_VERSION-libmodulemd glibc-langpack-en"
+  PKG_EXTRA="dnf-plugins-core desktop-file-utils flatpak ostree skopeo python$PYTHON_VERSION-libmodulemd glibc-langpack-en"
   BUILDDEP="dnf builddep"
   PYTHON="python$PYTHON_VERSION"
 else
@@ -41,7 +41,7 @@ else
   PIP="pip"
   PKG="yum"
   ENABLE_REPO=
-  PKG_EXTRA="yum-utils epel-release git-core desktop-file-utils flatpak ostree python2-libmodulemd2"
+  PKG_EXTRA="yum-utils epel-release git-core desktop-file-utils flatpak ostree skopeo python2-libmodulemd2"
   BUILDDEP="yum-builddep"
   PYTHON="python"
 fi

commit 404b389d9745b6fb0dfc4a924eee019a00d2bb36

Fix built Flatpak OCI workflow
After a flatpak OCI is built, it should be pushed into the internal
container storage for further analysis (built images are inspected for
metadata). This fixes an issue introduced in 917335d, where
atomic-reactor would try to inspect the image and fail since the image
would not be available.

Since the filesystem image used to build the OCI is not being tracked
anymore, this image would never get removed in the end of the build
process. Therefore, we align the image for removal and proper clean up.

* CLOUDBLD-434

Signed-off-by: Athos Ribeiro <athos@redhat.com>

atomic_reactor/constants.py
@@ -182,3 +182,6 @@ KOJI_KIND_IMAGE_SOURCE_BUILD = 'source_container_build'
 KOJI_SUBTYPE_OP_APPREGISTRY = 'operator_appregistry'
 KOJI_SUBTYPE_OP_BUNDLE = 'operator_bundle'
 KOJI_SOURCE_ENGINE = 'bsi'
+
+# Storage names as defined in skopeo
+DOCKER_STORAGE_TRANSPORT_NAME = 'docker-daemon'

atomic_reactor/inner.py
@@ -32,9 +32,11 @@ from atomic_reactor.plugin import (
     PrePublishPluginsRunner,
 )
 from atomic_reactor.source import get_source_instance_for, DummySource
-from atomic_reactor.constants import INSPECT_ROOTFS, INSPECT_ROOTFS_LAYERS
 from atomic_reactor.constants import (
     CONTAINER_DEFAULT_BUILD_METHOD,
+    DOCKER_STORAGE_TRANSPORT_NAME,
+    INSPECT_ROOTFS,
+    INSPECT_ROOTFS_LAYERS,
     PLUGIN_BUILD_ORCHESTRATE_KEY
 )
 from atomic_reactor.util import ImageName, exception_message
@@ -383,6 +385,7 @@ class DockerBuildWorkflow(object):
         self.built_image_inspect = None
         self.layer_sizes = []
         self.default_image_build_method = CONTAINER_DEFAULT_BUILD_METHOD
+        self.storage_transport = DOCKER_STORAGE_TRANSPORT_NAME
 
         # list of images pulled during the build, to be deleted after the build
         self.pulled_base_images = set()

atomic_reactor/plugins/prepub_flatpak_create_oci.py
@@ -19,10 +19,12 @@ from flatpak_module_tools.flatpak_builder import FlatpakBuilder, FLATPAK_METADAT
 
 from atomic_reactor.constants import IMAGE_TYPE_OCI, IMAGE_TYPE_OCI_TAR
 from atomic_reactor.plugin import PrePublishPlugin
+from atomic_reactor.plugins.exit_remove_built_image import defer_removal
 from atomic_reactor.plugins.pre_flatpak_update_dockerfile import get_flatpak_source_info
 from atomic_reactor.plugins.pre_reactor_config import get_flatpak_metadata
 from atomic_reactor.utils.rpm import parse_rpm_output
 from atomic_reactor.util import df_parser, get_exported_image_metadata
+from osbs.utils import Labels
 
 
 # This converts the generator provided by the export() operation to a file-like
@@ -102,6 +104,26 @@ class FlatpakCreateOciPlugin(PrePublishPlugin):
         oci_image_manifest = json.loads(raw_manifest)
         return oci_image_manifest['config']['digest']
 
+    def _copy_oci_to_local_storage(self, oci_path, name, tag):
+        """Copy OCI image to internal container storage
+
+        The internal storage to copy the image to is defined by the workflow.
+
+        :param oci_path: str, path to OCI directory
+        :param name: str, name to be given to the image in the internal storage
+        :param tag: str, tag to apply to name in the internal storage
+        """
+        skopeo_copy_dst = '{}:{}:{}'.format(self.workflow.storage_transport, name, tag)
+        cmd = ['skopeo', 'copy', 'oci:{}'.format(oci_path), skopeo_copy_dst]
+
+        self.log.info("Copying built image to internal container image storage as %s:%s", name, tag)
+        self.log.info("Calling: %s", ' '.join(cmd))
+        try:
+            subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as e:
+            self.log.error("image copy failed with output:\n%s", e.output)
+            raise
+
     def run(self):
         source = get_flatpak_source_info(self.workflow)
         if source is None:
@@ -124,9 +146,23 @@ class FlatpakCreateOciPlugin(PrePublishPlugin):
 
         ref_name, outfile, tarred_outfile = self.builder.build_container(tarred_filesystem)
 
+        self.log.info('Marking filesystem image "%s" for removal', self.workflow.builder.image_id)
+        defer_removal(self.workflow, self.workflow.builder.image_id)
+
         image_id = self._get_oci_image_id(outfile)
         self.log.info('New OCI image ID is %s', image_id)
         self.workflow.builder.image_id = image_id
+
+        labels = Labels(df_labels)
+        _, image_name = labels.get_name_and_value(Labels.LABEL_TYPE_NAME)
+        _, image_version = labels.get_name_and_value(Labels.LABEL_TYPE_VERSION)
+        _, image_release = labels.get_name_and_value(Labels.LABEL_TYPE_RELEASE)
+
+        name = '{}-{}'.format(self.key, image_name)
+        tag = '{}-{}'.format(image_version, image_release)
+        # The OCI id is tracked by the builder. The image will be removed in the exit phase
+        # No need to mark it for removal after pushing to the local storage
+        self._copy_oci_to_local_storage(outfile, name, tag)
 
         metadata = get_exported_image_metadata(outfile, IMAGE_TYPE_OCI)
         metadata['ref_name'] = ref_name

tests/plugins/test_flatpak_create_oci.py
@@ -469,6 +469,7 @@ def write_docker_file(config, tmpdir):
                     reason="libmodulemd not available")
 @pytest.mark.parametrize('config_name, flatpak_metadata, breakage', [
     ('app', 'annotations', None),
+    ('app', 'annotations', 'copy_error'),
     ('app', 'annotations', 'no_runtime'),
     ('app', 'labels', None),
     ('app', 'both', None),
@@ -507,6 +508,9 @@ def test_flatpak_create_oci(tmpdir, docker_tasker, config_name, flatpak_metadata
     setattr(workflow, 'builder', MockBuilder())
     workflow.builder.df_path = write_docker_file(config, str(tmpdir))
     setattr(workflow.builder, 'tasker', docker_tasker)
+
+    #  Make a local copy instead of pushing oci to docker storage
+    workflow.storage_transport = 'oci:{}'.format(str(tmpdir))
 
     make_and_store_reactor_config_map(workflow, flatpak_metadata)
 
@@ -550,6 +554,9 @@ def test_flatpak_create_oci(tmpdir, docker_tasker, config_name, flatpak_metadata
         module_config['metadata'] = mmd_index.dump_to_string()
 
         expected_exception = 'Failed to identify runtime module'
+    elif breakage == 'copy_error':
+        workflow.storage_transport = 'idontexist'
+        expected_exception = 'CalledProcessError'
     else:
         assert breakage is None
         expected_exception = None
@@ -602,9 +609,16 @@ def test_flatpak_create_oci(tmpdir, docker_tasker, config_name, flatpak_metadata
             runner.run()
         assert expected_exception in str(ex.value)
     else:
-        assert workflow.builder.image_id == 'xxx'
+        # Check if run replaces image_id and marks filesystem image for removal
+        filesystem_image_id = 'xxx'
+        for_removal = workflow.plugin_workspace.get(
+            'remove_built_image', {}).get('images_to_remove', [])
+        assert workflow.builder.image_id == filesystem_image_id
+        assert filesystem_image_id not in for_removal
         runner.run()
+        for_removal = workflow.plugin_workspace['remove_built_image']['images_to_remove']
         assert re.match(r'^sha256:\w{64}$', workflow.builder.image_id)
+        assert filesystem_image_id in for_removal
 
         dir_metadata = workflow.exported_image_sequence[-2]
         assert dir_metadata['type'] == IMAGE_TYPE_OCI