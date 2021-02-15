95e25b740fd266f116bc29e028858ea307074341
mkosiarc <mkosiarc@redhat.com>
2021-02-15 16:11:58 +0100

    Increase HTTP_MAX_RETRIES
    This should raise the maximum waiting time
    of request to ~ 750 seconds
    
    According to my calculations the sleep time between
    individual requests will be:
    [0s, 10s, 20s, 40s, 80s, 120s, 120s, 120s, 120s, 120s]
    
    based on formula:
    {backoff factor} * (2 ** ({number of total retries} - 1))
    (Sleep time between request will never be more than 120s)
    
    Signed-off-by: mkosiarc <mkosiarc@redhat.com>

--- atomic_reactor/constants.py
+++ atomic_reactor/constants.py
@@ -133,7 +133,7 @@ DOCKER_PUSH_MAX_RETRIES = 6
 # how many seconds should wait before another try of docker push
 DOCKER_PUSH_BACKOFF_FACTOR = 5
 # max retries for http requests
-HTTP_MAX_RETRIES = 3
+HTTP_MAX_RETRIES = 10
 # how many seconds should wait before another try of http request
 HTTP_BACKOFF_FACTOR = 5
 # http retries statuses
