27cb3fa4fc5bff08a6eb9f755f222bd4b9dabbf0
Harsh Modi <hmodi@redhat.com>
2021-04-26 13:53:20 -0400

    implement artifact download for fetch-artifacts-pnc
    This change will handle processing fetch-artifacts-pnc.yaml
    and download the artifacts specified.
    
    Signed-off-by: Harsh Modi <hmodi@redhat.com>

--- atomic_reactor/utils/pnc.py
+++ atomic_reactor/utils/pnc.py
@@ -5,6 +5,7 @@ All rights reserved.
 This software may be modified and distributed under the terms
 of the BSD license. See the LICENSE file for details.
 """
+import hashlib
 import os
 import re
 
@@ -18,9 +19,43 @@ class PNCUtil(object):
             self.session = get_retrying_requests_session()
         else:
             self.session = session
-        self.base_api_url = pnc_map['base_api_url']
-        # using urljoin here causes the API path in the base_api_url to be removed
-        self.get_scm_archive_request_url = self.base_api_url + '/' + pnc_map['get_scm_archive_path']
+
+        self.pnc_map = pnc_map
+        self.base_api_url = self.pnc_map['base_api_url']
+        self._artifact_request_url = None
+        self._scm_archive_request_url = None
+
+    @property
+    def artifact_request_url(self):
+        if not self._artifact_request_url:
+            # using urljoin here causes the API path in the base_api_url to be removed
+            self._artifact_request_url = self.base_api_url + '/' + self.pnc_map['get_artifact_path']
+        return self._artifact_request_url
+
+    @property
+    def scm_archive_request_url(self):
+        if not self._scm_archive_request_url:
+            # using urljoin here causes the API path in the base_api_url to be removed
+            self._scm_archive_request_url = (self.base_api_url + '/'
+                                             + self.pnc_map['get_scm_archive_path'])
+        return self._scm_archive_request_url
+
+    def get_artifact(self, artifact_id):
+        """
+        Return a URL to artifact and it's checksums
+        :param artifact_id: str PNC artifact id
+        :return str, dict; URL of the artifacts and it's checksums to verify download
+        :rtype str, dict; URL and checksums
+        """
+        response = self.session.get(self.artifact_request_url.format(artifact_id))
+        response.raise_for_status()
+
+        artifact = response.json()
+        url = artifact['publicUrl']
+        checksums = {algo: artifact[algo] for algo in hashlib.algorithms_guaranteed
+                     if algo in artifact}
+
+        return url, checksums
 
     def get_scm_archive_from_build_id(self, build_id: str):
         """
@@ -33,7 +68,7 @@ class PNCUtil(object):
         # This endpoint for the API is redirected to the actual source URL which
         #  we don't want to download yet, so we're disabling the redirect to just
         #  get the redirect location in response header
-        response = self.session.get(self.get_scm_archive_request_url.format(build_id),
+        response = self.session.get(self.scm_archive_request_url.format(build_id),
                                     allow_redirects=False)
         response.raise_for_status()
 
