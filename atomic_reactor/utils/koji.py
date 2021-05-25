containerbuildsystem/atomic-reactor

commit 24f4e03871bf01fc31e933558fbc7cd48e8dcf45

Export remote_sources metadata
Update the metadata format and prepare it for
using multiple remote sources
CLOUDBLD-4453

Signed-off-by: mkosiarc <mkosiarc@redhat.com>

atomic_reactor/utils/koji.py
@@ -24,7 +24,7 @@ from atomic_reactor.constants import (DEFAULT_DOWNLOAD_BLOCK_SIZE, PROG,
                                       PLUGIN_EXPORT_OPERATOR_MANIFESTS_KEY,
                                       PLUGIN_RESOLVE_REMOTE_SOURCE,
                                       PLUGIN_GENERATE_MAVEN_METADATA_KEY,
-                                      REMOTE_SOURCES_FILENAME, KOJI_MAX_RETRIES,
+                                      KOJI_MAX_RETRIES,
                                       KOJI_RETRY_INTERVAL, KOJI_OFFLINE_RETRY_INTERVAL)
 from atomic_reactor.util import (get_version_of_tools, get_docker_architecture,
                                  Output, get_image_upload_filename,
@@ -367,32 +367,32 @@ def get_image_output(workflow, image_id, arch):
     return metadata, output
 
 
-def get_source_tarball_output(workflow):
+def get_source_tarballs_output(workflow):
     plugin_results = workflow.prebuild_results.get(PLUGIN_RESOLVE_REMOTE_SOURCE) or {}
-    remote_source_path = plugin_results.get('remote_source_path')
-    if not remote_source_path:
-        return None
+    output_tarball_files = []
+    for remote_source in plugin_results:
+        remote_source_tarball = remote_source['remote_source_tarball']
+        metadata = get_output_metadata(remote_source_tarball['path'],
+                                       remote_source_tarball['filename'])
+        output = Output(file=open(remote_source_tarball['path']), metadata=metadata)
+        output_tarball_files.append(output)
+    return output_tarball_files
 
-    metadata = get_output_metadata(remote_source_path, REMOTE_SOURCES_FILENAME)
-    output = Output(file=open(remote_source_path), metadata=metadata)
-    return output
 
-
-def get_remote_source_json_output(workflow):
+def get_remote_sources_json_output(workflow):
     plugin_results = workflow.prebuild_results.get(PLUGIN_RESOLVE_REMOTE_SOURCE) or {}
-    remote_source_json = plugin_results.get('remote_source_json')
-    if not remote_source_json:
-        return None
-
-    remote_source_json_filename = 'remote-source.json'
+    output_json_files = []
     tmpdir = tempfile.mkdtemp()
-    file_path = os.path.join(tmpdir, remote_source_json_filename)
-    with open(file_path, 'w') as f:
-        json.dump(remote_source_json, f, indent=4, sort_keys=True)
-
-    metadata = get_output_metadata(file_path, remote_source_json_filename)
-    output = Output(file=open(file_path), metadata=metadata)
-    return output
+    for remote_source in plugin_results:
+        remote_source_json = remote_source['remote_source_json']
+        remote_source_json_filename = remote_source_json['filename']
+        file_path = os.path.join(tmpdir, remote_source_json_filename)
+        with open(file_path, 'w') as f:
+            json.dump(remote_source_json, f, indent=4, sort_keys=True)
+        metadata = get_output_metadata(file_path, remote_source_json_filename)
+        output = Output(file=open(file_path), metadata=metadata)
+        output_json_files.append(output)
+    return output_json_files
 
 
 def get_maven_metadata(workflow):

atomic_reactor/constants.py
@@ -183,7 +183,8 @@ KOJI_BTYPE_REMOTE_SOURCE_FILE = 'remote-source-file'
 REMOTE_SOURCE_DIR = '/remote-source'
 
 # Name of downloaded remote sources tarball
-REMOTE_SOURCES_FILENAME = 'remote-source.tar.gz'
+REMOTE_SOURCE_TARBALL_FILENAME = 'remote-source.tar.gz'
+REMOTE_SOURCE_JSON_FILENAME = 'remote-source.json'
 
 # koji osbs_build metadata
 KOJI_KIND_IMAGE_BUILD = 'container_build'

atomic_reactor/plugins/exit_koji_import.py
@@ -27,7 +27,7 @@ from atomic_reactor.utils.koji import get_buildroot as koji_get_buildroot
 from atomic_reactor.utils.koji import get_output as koji_get_output
 from atomic_reactor.utils.koji import (
         generate_koji_upload_dir, add_custom_type,
-        get_source_tarball_output, get_remote_source_json_output,
+        get_source_tarballs_output, get_remote_sources_json_output,
         get_maven_metadata
 )
 from atomic_reactor.plugins.pre_reactor_config import get_openshift_session
@@ -69,7 +69,11 @@ from atomic_reactor.util import (Output, get_build_json,
                                  has_operator_appregistry_manifest,
                                  )
 from atomic_reactor.utils.koji import (KojiUploadLogger, get_koji_task_owner)
-from atomic_reactor.plugins.pre_reactor_config import get_koji_session, get_koji
+from atomic_reactor.plugins.pre_reactor_config import (
+    get_koji_session,
+    get_koji,
+    get_allow_multiple_remote_sources,
+)
 from atomic_reactor.metadata import label
 from osbs.utils import Labels, ImageName
 
@@ -230,17 +234,38 @@ class KojiImportBase(ExitPlugin):
             extra['image']['pnc'] = pnc_build_metadata
 
     def set_remote_sources_metadata(self, extra):
-        remote_source_result = self.workflow.prebuild_results.get(PLUGIN_RESOLVE_REMOTE_SOURCE)
+        remote_source_result = self.workflow.prebuild_results.get(
+            PLUGIN_RESOLVE_REMOTE_SOURCE
+        )
         if remote_source_result:
-            url = remote_source_result['annotations']['remote_source_url']
-            remote_source_typeinfo = {
-                KOJI_BTYPE_REMOTE_SOURCES: {
-                    'remote_source_url': url,
-                },
-            }
-            extra.setdefault('typeinfo', {}).update(remote_source_typeinfo)
+            if get_allow_multiple_remote_sources(self.workflow):
+                remote_sources_image_metadata = [
+                    {"name": remote_source["name"], "url": remote_source["url"].rstrip('/download')}
+                    for remote_source in remote_source_result
+                ]
+                extra["image"]["remote_sources"] = remote_sources_image_metadata
 
-            extra['image']['remote_source_url'] = url
+                remote_sources_typeinfo_metadata = [
+                    {
+                        "name": remote_source["name"],
+                        "url": remote_source["url"].rstrip('/download'),
+                        "archives": [
+                            remote_source["remote_source_json"]["filename"],
+                            remote_source["remote_source_tarball"]["filename"],
+                        ],
+                    }
+                    for remote_source in remote_source_result
+                ]
+            else:
+                extra["image"]["remote_source_url"] = remote_source_result[0]["url"]
+                remote_sources_typeinfo_metadata = {
+                    "remote_source_url": remote_source_result[0]["url"]
+                }
+
+            remote_source_typeinfo = {
+                KOJI_BTYPE_REMOTE_SOURCES: remote_sources_typeinfo_metadata,
+            }
+            extra.setdefault("typeinfo", {}).update(remote_source_typeinfo)
 
     def set_remote_source_file_metadata(self, extra):
         plugin_results = self.workflow.postbuild_results.get(PLUGIN_GENERATE_MAVEN_METADATA_KEY) \
@@ -425,10 +450,10 @@ class KojiImportBase(ExitPlugin):
             if worker_output['type'] == 'docker-image':
                 worker_output['components'] += kojifile_components
 
-        # add remote source tarball and remote-source.json files to output
+        # add remote sources tarballs and remote sources json files to output
         for remote_source_output in [
-            get_source_tarball_output(self.workflow),
-            get_remote_source_json_output(self.workflow)
+            *get_source_tarballs_output(self.workflow),
+            *get_remote_sources_json_output(self.workflow)
         ]:
             if remote_source_output:
                 add_custom_type(remote_source_output, KOJI_BTYPE_REMOTE_SOURCES)

atomic_reactor/plugins/exit_store_metadata_in_osv3.py
@@ -195,10 +195,14 @@ class StoreMetadataInOSv3Plugin(ExitPlugin):
 
     def apply_remote_source_annotations(self, annotations):
         try:
-            rs_annotations = self.get_pre_result(PLUGIN_RESOLVE_REMOTE_SOURCE)['annotations']
+            remote_sources = self.get_pre_result(PLUGIN_RESOLVE_REMOTE_SOURCE)
+            remote_sources_annotations = [
+                {"name": remote_source["name"], "url": remote_source["url"]}
+                for remote_source in remote_sources
+            ]
         except (TypeError, KeyError):
             return
-        annotations.update(rs_annotations)
+        annotations.update(remote_sources_annotations)
 
     def run(self):
         metadata = get_build_json().get("metadata", {})

atomic_reactor/plugins/pre_fetch_sources.py
@@ -28,6 +28,7 @@ from atomic_reactor.util import get_retrying_requests_session
 from atomic_reactor.download import download_url
 from atomic_reactor.metadata import label_map
 from atomic_reactor.utils.pnc import PNCUtil
+from atomic_reactor.constants import REMOTE_SOURCE_JSON_FILENAME
 
 
 @label_map('sources_for_koji_build_id')
@@ -236,7 +237,8 @@ class FetchSourcesPlugin(PreBuildPlugin):
                 dest_name = remote_source['dest']
                 remote_sources_urls.append(remote_source)
 
-            elif archive['type_name'] == 'json' and archive['filename'] == 'remote-source.json':
+            elif (archive['type_name'] == 'json'
+                  and archive['filename'] == REMOTE_SOURCE_JSON_FILENAME):
                 cachito_json_url = os.path.join(remote_sources_path, archive['filename'])
 
         # we will add entry to remote_json_map only when

atomic_reactor/plugins/pre_reactor_config.py
@@ -160,6 +160,10 @@ def get_cachito_session(workflow):
     return CachitoAPI(config['api_url'], **api_kwargs)
 
 
+def get_allow_multiple_remote_sources(workflow):
+    return get_value(workflow, 'allow_multiple_remote_sources', False)
+
+
 def get_arrangement_version(workflow, fallback=NO_FALLBACK):
     return get_value(workflow, 'arrangement_version', fallback)
 

atomic_reactor/plugins/pre_resolve_remote_source.py
@@ -7,12 +7,17 @@ of the BSD license. See the LICENSE file for details.
 """
 import os.path
 
-from atomic_reactor.constants import PLUGIN_RESOLVE_REMOTE_SOURCE, REMOTE_SOURCE_DIR
+from atomic_reactor.constants import (
+    PLUGIN_RESOLVE_REMOTE_SOURCE,
+    REMOTE_SOURCE_DIR,
+    REMOTE_SOURCE_JSON_FILENAME,
+    REMOTE_SOURCE_TARBALL_FILENAME,
+)
 from atomic_reactor.utils.koji import get_koji_task_owner
 from atomic_reactor.plugin import PreBuildPlugin
 from atomic_reactor.plugins.build_orchestrate_build import override_build_kwarg
 from atomic_reactor.plugins.pre_reactor_config import (
-    get_cachito, get_cachito_session, get_koji_session)
+    get_cachito, get_cachito_session, get_koji_session, get_allow_multiple_remote_sources)
 from atomic_reactor.util import get_build_json, is_scratch_build
 
 
@@ -70,7 +75,8 @@ class ResolveRemoteSourcePlugin(PreBuildPlugin):
             self.log.info('Aborting plugin execution: missing Cachito configuration')
             return
 
-        if self.workflow.source.config.remote_sources:
+        if (not get_allow_multiple_remote_sources(self.workflow)
+                and self.workflow.source.config.remote_sources):
             raise ValueError('Multiple remote sources are not supported, use single '
                              'remote source in container.yaml')
 
@@ -107,14 +113,20 @@ class ResolveRemoteSourcePlugin(PreBuildPlugin):
         dest_dir = self.workflow.source.workdir
         dest_path = self.cachito_session.download_sources(source_request, dest_dir=dest_dir)
 
-        return {
-            # Annotations to be added to the current Build object
-            'annotations': {'remote_source_url': remote_sources[0]['url']},
-            # JSON representation of the remote source request
-            'remote_source_json': remote_source_json,
-            # Local path to the remote source archive
-            'remote_source_path': dest_path,
-        }
+        return [
+            {
+                "name": remote_sources[0]["name"],
+                "url": remote_sources[0]["url"],
+                "remote_source_json": {
+                    "json": remote_source_json,
+                    "filename": REMOTE_SOURCE_JSON_FILENAME,
+                },
+                "remote_source_tarball": {
+                    "filename": REMOTE_SOURCE_TARBALL_FILENAME,
+                    "path": dest_path,
+                },
+            }
+        ]
 
     def set_worker_params(self, remote_sources):
         build_args = {}

atomic_reactor/utils/cachito.py
@@ -12,7 +12,7 @@ import logging
 import requests
 import time
 
-from atomic_reactor.constants import REMOTE_SOURCES_FILENAME
+from atomic_reactor.constants import REMOTE_SOURCE_TARBALL_FILENAME
 from atomic_reactor.download import download_url
 from atomic_reactor.util import get_retrying_requests_session
 
@@ -168,7 +168,7 @@ class CachitoAPI(object):
                 else:
                     time.sleep(burst_retry)
 
-    def download_sources(self, request, dest_dir='.', dest_filename=REMOTE_SOURCES_FILENAME):
+    def download_sources(self, request, dest_dir='.', dest_filename=REMOTE_SOURCE_TARBALL_FILENAME):
         """Download the sources from a Cachito request
 
         :param request: int or dict, either the Cachito request ID or a dict with 'id' key

commit 647033837fa9c1b31fff17a146fb86f3d1586fe0

Fix reference to raw json for remote-source.json file
A bug introduced in https://github.com/containerbuildsystem/atomic-reactor/pull/1624
caused that remote-source.json file in koji had unexpected content

Signed-off-by: mkosiarc <mkosiarc@redhat.com>

atomic_reactor/utils/koji.py
@@ -388,7 +388,7 @@ def get_remote_sources_json_output(workflow):
         remote_source_json_filename = remote_source_json['filename']
         file_path = os.path.join(tmpdir, remote_source_json_filename)
         with open(file_path, 'w') as f:
-            json.dump(remote_source_json, f, indent=4, sort_keys=True)
+            json.dump(remote_source_json['json'], f, indent=4, sort_keys=True)
         metadata = get_output_metadata(file_path, remote_source_json_filename)
         output = Output(file=open(file_path), metadata=metadata)
         output_json_files.append(output)