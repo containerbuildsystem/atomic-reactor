ef14f3a2abdb7a255441df06bdc65020d8fd1411
Harsh Modi <hmodi@redhat.com>
2021-08-11 09:20:55 -0400

    add purl-spec for middleware artifacts
    For Product Security to be able to scan middleware artifacts
    for any vulnerabilities, they must be added to the image
    content manifest. This change will add all artifacts fetched
    via fetch-artifacts-pnc to the manifest.
    
    All artifacts are identified by their Package URLs(aka purl-specs)
    
    Signed-off-by: Harsh Modi <hmodi@redhat.com>

--- tests/plugins/test_add_image_content_manifest.py
+++ tests/plugins/test_add_image_content_manifest.py
@@ -20,7 +20,7 @@ from tests.mock_env import MockEnv
 from tests.utils.test_cachito import CACHITO_URL, CACHITO_REQUEST_ID
 
 from atomic_reactor import util
-from atomic_reactor.constants import INSPECT_ROOTFS, INSPECT_ROOTFS_LAYERS
+from atomic_reactor.constants import INSPECT_ROOTFS, INSPECT_ROOTFS_LAYERS, PLUGIN_FETCH_MAVEN_KEY
 from atomic_reactor.plugin import PluginFailedException
 from atomic_reactor.plugins.pre_add_image_content_manifest import AddImageContentManifestPlugin
 
@@ -33,6 +33,16 @@ CONTENT_SETS = {
 }
 CACHITO_ICM_URL = '{}/api/v1/content-manifest?requests={}'.format(CACHITO_URL,
                                                                   CACHITO_REQUEST_ID)
+PNC_ARTIFACT = {
+            'id': 1234,
+            'publicUrl': 'http://test.com/artifact.jar',
+            'md5': 'abcd',
+            'sha1': 'abcd',
+            'sha256': 'abcd',
+            'purl': 'pkg:maven/org.example.artifact/artifact-common@0.0.1.redhat-00001?type=jar',
+        }
+PNC_ROOT = 'http://pnc.example.com/pnc-rest/v2'
+PNC_ARTIFACT_URL = f"{PNC_ROOT}/artifacts/{PNC_ARTIFACT['id']}"
 ICM_MINIMAL_DICT = {
     'metadata': {
         'icm_version': 1,
@@ -67,6 +77,9 @@ ICM_DICT = {
                 },
             ]
         },
+        {
+            'purl': PNC_ARTIFACT['purl'],
+        }
     ]
 }
 ICM_JSON = dedent(
@@ -128,10 +141,11 @@ def mock_content_sets_config(tmpdir, empty=False):
 
 def mock_get_icm(requests_mock):
     requests_mock.register_uri('GET', CACHITO_ICM_URL, text=ICM_JSON)
+    requests_mock.register_uri('GET', PNC_ARTIFACT_URL, text=json.dumps(PNC_ARTIFACT))
 
 
 def mock_env(tmpdir, docker_tasker, platform='x86_64', base_layers=0,
-             remote_sources=REMOTE_SOURCES, r_c_m_override=None,
+             remote_sources=REMOTE_SOURCES, r_c_m_override=None, pnc_artifacts=True,
              ):  # pylint: disable=W0102
     inspection_data = {
         INSPECT_ROOTFS: {
@@ -147,6 +161,10 @@ def mock_env(tmpdir, docker_tasker, platform='x86_64', base_layers=0,
                     'ssl_certs_dir': str(tmpdir),
                 },
             },
+            'pnc': {
+                'base_api_url': PNC_ROOT,
+                'get_artifact_path': 'artifacts/{}',
+            },
         }
     else:
         r_c_m = r_c_m_override
@@ -157,6 +175,10 @@ def mock_env(tmpdir, docker_tasker, platform='x86_64', base_layers=0,
            .set_reactor_config(r_c_m)
            .make_orchestrator()
            )
+    if pnc_artifacts:
+        env.set_plugin_result(
+            'prebuild', PLUGIN_FETCH_MAVEN_KEY, {'pnc_artifact_ids': [PNC_ARTIFACT['id']]}
+        )
     tmpdir.join('cert').write('')
     env.workflow.builder.set_inspection_data(inspection_data)
     env.workflow.user_params['platform'] = platform
@@ -249,6 +271,34 @@ def test_add_image_content_manifest(requests_mock, tmpdir, docker_tasker, caplog
     assert expected_output == output
 
 
+def test_fetch_maven_artifacts_no_pnc_config(requests_mock, tmpdir, docker_tasker, caplog,
+                                             user_params):
+    mock_get_icm(requests_mock)
+    dfp = util.df_parser(str(tmpdir))
+    dfp.content = dedent("""\
+                            FROM base_image
+                            CMD build /spam/eggs
+                            LABEL com.redhat.component=eggs version=1.0 release=42
+                        """)
+    r_c_m = {
+        'version': 1,
+        'cachito': {
+            'api_url': CACHITO_URL,
+            'auth': {
+                'ssl_certs_dir': str(tmpdir),
+            },
+        },
+    }
+
+    with pytest.raises(PluginFailedException):
+        runner = mock_env(tmpdir, docker_tasker, r_c_m_override=r_c_m)
+        runner.workflow.builder.set_df_path(dfp.dockerfile_path)
+        runner.run()
+
+    msg = 'No PNC configuration found in reactor config map'
+    assert msg in caplog.text
+
+
 @pytest.mark.parametrize('content_sets', [True, False])
 @pytest.mark.parametrize(
     ('df_content, base_layers, manifest_file'), [
@@ -273,6 +323,41 @@ def test_none_remote_source_icm_url(requests_mock, tmpdir, docker_tasker, caplog
     runner = mock_env(tmpdir, docker_tasker, platform, base_layers, remote_sources=None)
     runner.workflow.builder.set_df_path(dfp.dockerfile_path)
     expected_output = deepcopy(ICM_MINIMAL_DICT)
+    expected_output['image_contents'].append({'purl': PNC_ARTIFACT['purl']})
+    if content_sets:
+        expected_output['content_sets'] = CONTENT_SETS[platform]
+    expected_output['metadata']['image_layer_index'] = base_layers
+    runner.run()
+    output_file = os.path.join(str(tmpdir), manifest_file)
+    with open(output_file) as f:
+        json_data = f.read()
+    output = json.loads(json_data)
+    assert expected_output == output
+
+
+@pytest.mark.parametrize('content_sets', [True, False])
+@pytest.mark.parametrize(
+    ('df_content, base_layers, manifest_file'), [
+        (
+            dedent("""\
+            FROM base_image
+            CMD build /spam/eggs
+            LABEL com.redhat.component=eggs version=1.0 release=42
+        """),
+            2,
+            'eggs-1.0-42.json',
+        )])
+def test_no_pnc_artifacts(requests_mock, tmpdir, docker_tasker, caplog, content_sets, df_content,
+                          base_layers, manifest_file):
+    platform = 'x86_64'
+    mock_get_icm(requests_mock)
+    mock_content_sets_config(tmpdir, empty=(not content_sets))
+    dfp = util.df_parser(str(tmpdir))
+    dfp.content = df_content
+    runner = mock_env(tmpdir, docker_tasker, platform, base_layers, pnc_artifacts=False)
+    runner.workflow.builder.set_df_path(dfp.dockerfile_path)
+    expected_output = deepcopy(ICM_DICT)
+    expected_output['image_contents'] = expected_output['image_contents'][:-1]
     if content_sets:
         expected_output['content_sets'] = CONTENT_SETS[platform]
     expected_output['metadata']['image_layer_index'] = base_layers
