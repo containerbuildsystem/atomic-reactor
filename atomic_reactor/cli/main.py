06183e4cf0dbb99755e3be3063d2d2d2059d9169
Robert Cerven <rcerven@redhat.com>
2021-09-17 16:29:23 +0200

    Remove build subparser
    * CLOUDBLD-6299
    
    Signed-off-by: Robert Cerven <rcerven@redhat.com>

--- atomic_reactor/cli/main.py
+++ atomic_reactor/cli/main.py
@@ -6,22 +6,18 @@ This software may be modified and distributed under the terms
 of the BSD license. See the LICENSE file for details.
 """
 
-import json
 import argparse
 import logging
-import os
 import sys
 import pkg_resources
 import locale
 
 from atomic_reactor import set_logging
 from osbs import set_logging as set_logging_osbs
-from atomic_reactor.api import (build_image_here, build_image_in_privileged_container,
-                                build_image_using_hosts_docker)
 from atomic_reactor.constants import DESCRIPTION, PROG
 from atomic_reactor.buildimage import BuildImageBuilder
-from atomic_reactor.inner import build_inside, BuildResults
-from atomic_reactor.util import (process_substitutions, setup_introspection_signal_handler,
+from atomic_reactor.inner import build_inside
+from atomic_reactor.util import (setup_introspection_signal_handler,
                                  exception_message)
 
 
@@ -39,33 +35,6 @@ def cli_create_build_image(args):
         logger.error("build failed")
         sys.exit(1)
     sys.exit(0)
-
-
-def cli_build_image(args):
-    if args.plugin_files:
-        args.plugin_files = [os.path.abspath(f) for f in args.plugin_files]
-    if args.source__provider == 'json':
-        with open(args.json_path) as json_fp:
-            common_kwargs = json.load(json_fp)
-        if args.substitute:
-            process_substitutions(common_kwargs, args.substitute)
-    else:
-        common_kwargs = construct_kwargs(**vars(args))
-    response = BuildResults()
-    if args.method == "hostdocker":
-        response = build_image_using_hosts_docker(args.build_image, **common_kwargs)
-    elif args.method == "privileged":
-        response = build_image_in_privileged_container(args.build_image, **common_kwargs)
-    elif args.method == 'here':
-        build_result = build_image_here(plugin_files=args.plugin_files, **common_kwargs)
-        if build_result.is_failed():
-            response.return_code = -1
-        else:
-            response.return_code = 0
-
-    if response.return_code != 0:
-        logger.error("build failed")
-    sys.exit(response.return_code)
 
 
 def construct_kwargs(**kwargs):
@@ -103,7 +72,6 @@ class CLI(object):
             description=DESCRIPTION,
             formatter_class=formatter_class,
         )
-        self.build_parser = None
         self.bi_parser = None
         self.ib_parser = None
         self.source_types_parsers = None
@@ -122,29 +90,6 @@ class CLI(object):
         exclusive_group.add_argument("-V", "--version", action="version", version=version)
 
         subparsers = self.parser.add_subparsers(help='commands')
-
-        # BUILDING IMAGES
-
-        self.build_parser = subparsers.add_parser(
-            'build',
-            usage="%s [OPTIONS] build" % PROG,
-            description='This command enables you to build images. '
-                        'Currently, you can build images from git repo, local path or '
-                        'a json file with full build specification. '
-                        'There are several methods for performing the build: '
-                        'inside a build container using docker from host, '
-                        'inside a build container using new instance of docker, '
-                        'or within current environment.'
-        )
-
-        def cli_build_with_source(args):
-            if args.source__provider is None:
-                self.build_parser.print_help()
-            else:
-                cli_build_image(args)
-
-        self.build_parser.set_defaults(func=cli_build_with_source)
-        self.generate_source_types_subparsers()
 
         # CREATE BUILD IMAGE
 
@@ -195,89 +140,6 @@ class CLI(object):
                                          "plugin_type.plugin_name.key=value)")
         self.ib_parser.set_defaults(func=cli_inside_build)
 
-    def generate_source_types_subparsers(self):
-        build_subparsers = self.build_parser.add_subparsers(help='select source provider to use',
-                                                            dest='source__provider')
-        self.source_types_parsers = {
-            'git': build_subparsers.add_parser(
-                'git',
-                help='build from a git repo',
-                usage="%s [OPTIONS] build git" % PROG),
-            'path': build_subparsers.add_parser(
-                'path',
-                help='build from a local path',
-                usage="%s [OPTIONS] build path" % PROG),
-        }
-        source_types_uri_help = {
-            'git': 'URI of the git repo',
-            'path': 'URI of the directory with source (e.g. "file:///foo/bar") or\
-                    path (e.g. "/foo/bar"); path can be relative',
-        }
-        source_types_df_path_help = {
-            'git': 'path to Dockerfile within git repo (default is ./)',
-            'path': 'path to Dockerfile within given directory (default is ./)',
-        }
-
-        # add arguments that are common to non-json source types
-        for st, stp in self.source_types_parsers.items():
-            stp.add_argument(
-                "--image", action='store', required=True,
-                help="name under which the image will be accessible")
-            stp.add_argument(
-                "--uri", action='store', metavar="URI", required=True,
-                help=source_types_uri_help[st], dest='source__uri')
-            stp.add_argument(
-                "--df-path", action='store', help=source_types_df_path_help[st],
-                dest='source__dockerfile_path')
-            stp.add_argument(
-                "--source-registry", action='store', metavar="REGISTRY",
-                help="registry to pull base image from")
-            stp.add_argument(
-                "--source-registry-insecure", action='store_true',
-                help="allow connecting to source registry over plain http")
-            stp.add_argument(
-                "--target-registries", action='store', nargs="*", metavar="REGISTRY",
-                help="list of registries to push image to")
-            stp.add_argument(
-                "--target-registries-insecure", action='store_true',
-                help="allow connecting to target registries over plain http")
-            stp.add_argument(
-                "--dont-pull-base-image", action='store_true',
-                help="don't pull or update base image specified in dockerfile")
-
-        # add the "json" subparser that allows to build from a json build file
-        self.source_types_parsers['json'] = build_subparsers.add_parser(
-            'json',
-            help='load build configuration from json file',
-            usage="%s [OPTIONS] build json" % PROG)
-
-        # add arguments common to both non-json and json source types
-        for stp in self.source_types_parsers.values():
-            stp.add_argument(
-                "--build-image", action='store',
-                help="name of build image to use (build image type has to match method)")
-            stp.add_argument(
-                "--load-plugin", action="store", nargs="*", metavar="PLUGIN_FILE",
-                dest="plugin_files", help="list of files where plugins live")
-            stp.add_argument(
-                "--method", action='store', required=True,
-                choices=["hostdocker", "privileged", "here"],
-                help="choose method for building image: "
-                     "'hostdocker' mounts socket inside privileged container to use docker from "
-                     "host, 'privileged' runs separate docker instance inside privileged "
-                     "container and 'here' executes build in current environment")
-
-        # add sourcetype-specific arguments now
-        self.source_types_parsers['git'].add_argument(
-            '--commit', action='store',
-            dest='source__provider_params__git_commit',
-            help="checkout this commit (default is master)")
-        self.source_types_parsers['json'].add_argument(
-            'json_path', metavar='JSON_PATH',
-            help='path to the build json')
-        self.source_types_parsers['json'].add_argument(
-            '--substitute', nargs='*', metavar='SUBSTITUTE',
-            help='provide substitutions for json in form "foo.bar=spam"')
 
     def run(self):
         self.set_arguments()
