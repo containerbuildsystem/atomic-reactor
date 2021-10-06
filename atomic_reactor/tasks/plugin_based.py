b7bea8c4ac282a5b9a581c1fe1779e3850d32ca8
Adam Cmiel <acmiel@redhat.com>
2021-10-06 12:00:43 +0200

    Change workflow init to better support tasks
    CLOUDBLD-6300
    
    Change the 'source' argument from a dict to a Source instance.
    
    Replace all the *_plugins arguments with a single 'plugins' argument
    (which takes a PluginsDef instance).
    
    Add 'user_params' and 'reactor_config_path' arguments.
    
    Move the PluginsDef definition to make it importable in the 'inner'
    module without causing cyclic imports later.
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- atomic_reactor/tasks/plugin_based.py
+++ atomic_reactor/tasks/plugin_based.py
@@ -6,33 +6,12 @@ This software may be modified and distributed under the terms
 of the BSD license. See the LICENSE file for details.
 """
 
-from dataclasses import dataclass, field
-from typing import List
-
-from atomic_reactor import util
 from atomic_reactor.tasks import common
+from atomic_reactor.tasks import PluginsDef
 
-
-@dataclass(frozen=True)
-class PluginsDef:
-    """Defines the plugins to be executed by a task."""
-
-    prebuild: List[dict] = field(default_factory=list)
-    buildstep: List[dict] = field(default_factory=list)
-    prepublish: List[dict] = field(default_factory=list)
-    postbuild: List[dict] = field(default_factory=list)
-    exit: List[dict] = field(default_factory=list)
-
-    def __post_init__(self):
-        """Validate the plugin definition right after the instance is created."""
-        to_validate = {
-            "prebuild_plugins": self.prebuild,
-            "buildstep_plugins": self.buildstep,
-            "prepublish_plugins": self.prepublish,
-            "postbuild_plugins": self.postbuild,
-            "exit_plugins": self.exit,
-        }
-        util.validate_with_schema(to_validate, "schemas/plugins.json")
+# PluginsDef can be considered as part of this module, but is defined elsewhere to avoid cyclic
+#   imports between the `inner` module and this module
+__all__ = ["PluginsDef", "PluginBasedTask"]
 
 
 class PluginBasedTask(common.Task):
