3c5f772e9587293a95ac4331ea4fee4607d7e859
Adam Cmiel <acmiel@redhat.com>
2021-10-06 12:10:33 +0200

    Implement plugin based task execution
    CLOUDBLD-6300
    
    Implements the remaining logic to make tasks executable. It is loosely
    based on the inner.build_inside() method from OSBS 1.x
    
    The build_inside() method used to run an input plugin, create a workflow
    instance from the results of the input plugin and execute the workflow.
    Input plugins no longer exist, now the workflow instance is instead
    created based on the task parameters and a hardcoded plugins definition.
    
    The input_osv3 plugin also used to remove some plugins if their required
    configuration was not present in the config file:
    
    - koji_* plugins if no koji config
    - resolve_composes if no odcs config
    - sendmail if no smtp config
    - distgit_fetch_artefacts if no sources_command
    
    That is no longer necessary. For the koji plugins, the configuration is
    now required by the schema, the others have sane handling for missing
    configs.
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- atomic_reactor/tasks/plugin_based.py
+++ atomic_reactor/tasks/plugin_based.py
@@ -6,12 +6,17 @@ This software may be modified and distributed under the terms
 of the BSD license. See the LICENSE file for details.
 """
 
+import logging
+
+from atomic_reactor import inner
 from atomic_reactor.tasks import common
 from atomic_reactor.tasks import PluginsDef
 
 # PluginsDef can be considered as part of this module, but is defined elsewhere to avoid cyclic
 #   imports between the `inner` module and this module
 __all__ = ["PluginsDef", "PluginBasedTask"]
+
+logger = logging.getLogger(__name__)
 
 
 class PluginBasedTask(common.Task):
@@ -22,4 +27,26 @@ class PluginBasedTask(common.Task):
 
     def execute(self):
         """Execute the plugins defined in plugins_def."""
-        raise NotImplementedError("This task is not yet implemented")
+        workflow = inner.DockerBuildWorkflow(
+            source=self._params.source,
+            plugins=self.plugins_def,
+            user_params=self._params.user_params,
+            reactor_config_path=self._params.config_file,
+            # OSBS2 TBD: openshift_build_selflink=?
+        )
+
+        try:
+            result = workflow.build_docker_image()
+        except Exception as e:
+            logger.error("task failed: %s", e)
+            raise
+
+        if result.is_failed():
+            msg = f"task failed: {result.fail_reason}"
+            logger.error(msg)
+            raise RuntimeError(msg)
+
+        # OSBS2 TBD: OSBS used to log the original Dockerfile after executing the workflow.
+        #   It probably doesn't make sense to do that here, but it would be good to log the
+        #   Dockerfile somewhere at the end of the build process.
+        logger.info(r"task finished successfully \o/")
