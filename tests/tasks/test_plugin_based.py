9947daf7b65ea6eeb3f320b7b6230e8c7806fe42
Adam Cmiel <acmiel@redhat.com>
2021-10-06 12:10:33 +0200

    Implement plugin based task execution
    CLOUDBLD-6300
    
    Implements the remaining logic to make tasks executable. It is loosely
    based on the inner.build_inside() method from OSBS 1.x
    
    The build_inside() method used to run an input plugin, create a workflow
    instance from the results of the input plugin and execute the workflow.
    Input plugins no longer exist, now the workflow instance is instead
    created based on the task parameters and a hardcoded plugins definition.
    
    The input_osv3 plugin also used to remove some plugins if their required
    configuration was not present in the config file:
    
    - koji_* plugins if no koji config
    - resolve_composes if no odcs config
    - sendmail if no smtp config
    - distgit_fetch_artefacts if no sources_command
    
    That is no longer necessary. For the koji plugins, the configuration is
    now required by the schema, the others have sane handling for missing
    configs.
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- tests/tasks/test_plugin_based.py
+++ tests/tasks/test_plugin_based.py
@@ -6,10 +6,12 @@ This software may be modified and distributed under the terms
 of the BSD license. See the LICENSE file for details.
 """
 
+import flexmock
 import pytest
 
 from osbs.exceptions import OsbsValidationException
 
+from atomic_reactor import inner
 from atomic_reactor.tasks import plugin_based
 
 
@@ -27,3 +29,72 @@ class TestPluginsDef:
     def test_create_invalid(self):
         with pytest.raises(OsbsValidationException, match="1 is not of type 'boolean'"):
             plugin_based.PluginsDef(prebuild=[{"name": "some_plugin", "required": 1}])
+
+
+class TestPluginBasedTask:
+    """Tests for the PluginBasedTask class"""
+
+    @pytest.fixture
+    def task_with_mocked_deps(self, monkeypatch):
+        """Create a PluginBasedTask instance with mocked task parameters.
+
+        Mock DockerBuildWorkflow accordingly. Return the mocked workflow instance for further
+        customization in individual tests.
+        """
+        expect_source = flexmock()
+        task_params = flexmock(
+            source=expect_source, user_params={"a": "b"}, config_file="config.yaml"
+        )
+
+        expect_plugins = flexmock()
+        monkeypatch.setattr(plugin_based.PluginBasedTask, "plugins_def", expect_plugins)
+
+        mocked_workflow = flexmock(inner.DockerBuildWorkflow)
+        (
+            mocked_workflow
+            .should_receive("__init__")
+            .once()
+            .with_args(
+                source=expect_source,
+                plugins=expect_plugins,
+                user_params={"a": "b"},
+                reactor_config_path="config.yaml",
+            )
+        )
+        mocked_workflow.should_receive("build_docker_image").and_raise(
+            AssertionError("you must mock the build_docker_image() workflow method")
+        )
+
+        task = plugin_based.PluginBasedTask(task_params)
+        return task, mocked_workflow
+
+    def test_execute(self, task_with_mocked_deps, caplog):
+        task, mocked_workflow = task_with_mocked_deps
+
+        success = inner.BuildResult()
+        mocked_workflow.should_receive("build_docker_image").and_return(success)
+
+        task.execute()
+        assert r"task finished successfully \o/" in caplog.text
+
+    def test_execute_raises_exception(self, task_with_mocked_deps, caplog):
+        task, mocked_workflow = task_with_mocked_deps
+
+        error = ValueError("something went wrong")
+        mocked_workflow.should_receive("build_docker_image").and_raise(error)
+
+        with pytest.raises(ValueError, match="something went wrong"):
+            task.execute()
+
+        assert "task failed: something went wrong" in caplog.text
+
+    def test_execute_returns_failure(self, task_with_mocked_deps, caplog):
+        task, mocked_workflow = task_with_mocked_deps
+
+        failure = inner.BuildResult(fail_reason="workflow returned failure")
+        mocked_workflow.should_receive("build_docker_image").and_return(failure)
+
+        with pytest.raises(RuntimeError, match="task failed: workflow returned failure"):
+            task.execute()
+
+        assert "task failed: workflow returned failure" in caplog.text
