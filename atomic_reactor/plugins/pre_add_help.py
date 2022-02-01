1ef290b8e50ba57b28d0b8ae80feef18e5d41b16
Adam Cmiel <acmiel@redhat.com>
2022-02-01 11:14:10 +0100

    Update add_help plugin for Tekton
    CLOUDBLD-8123
    
    Copy the modified help file and generated man file to all build dirs.
    Update the Dockerfile in each build dir.
    
    Update tests accordingly, also take the opportunity to make the mocking
    more consistent.
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- atomic_reactor/plugins/pre_add_help.py
+++ atomic_reactor/plugins/pre_add_help.py
@@ -18,12 +18,14 @@ Example configuration:
 """
 
 import errno
-import os
-from subprocess import check_output, CalledProcessError, STDOUT
 from datetime import datetime as dt
+from pathlib import Path
+from subprocess import check_output, CalledProcessError, STDOUT
+from typing import List
+
 from atomic_reactor import start_time as atomic_reactor_start_time
+from atomic_reactor.dirs import BuildDir
 from atomic_reactor.plugin import PreBuildPlugin
-from atomic_reactor.util import df_parser
 from atomic_reactor.metadata import annotation_map
 from osbs.utils import Labels
 
@@ -57,23 +59,35 @@ class AddHelpPlugin(PreBuildPlugin):
         self.NO_HELP_FILE_FOUND if no help found
         or self.HELP_GENERATED if help man page was generated
         """
-
-        help_path = os.path.join(self.workflow.df_dir, self.help_file)
-
-        if not os.path.exists(help_path):
-            self.log.info("File %s not found", help_path)
+        if not (self.workflow.build_dir.any_platform.path / self.help_file).exists():
+            self.log.info("File %s not present in the build directory", self.help_file)
             return {
                 'help_file': None,
                 'status': self.NO_HELP_FILE_FOUND
             }
 
-        dockerfile = df_parser(self.workflow.df_path, workflow=self.workflow)
+        self.workflow.build_dir.for_all_platforms_copy(self.render_help_file)
+        self.workflow.build_dir.for_each_platform(self.add_help_file_to_df)
+
+        return {
+            'help_file': self.help_file,
+            'status': self.HELP_GENERATED
+        }
+
+    def render_help_file(self, build_dir: BuildDir) -> List[Path]:
+        """Update the help.md file in the build directory and use it to generate a man file."""
+        dockerfile = build_dir.dockerfile_with_parent_env(
+            # platform should not matter, we only care about the component and maintainer labels
+            self.workflow.imageutil.base_image_inspect()
+        )
         labels = Labels(dockerfile.labels)
         try:
             _, name = labels.get_name_and_value(Labels.LABEL_TYPE_NAME)
         except KeyError:
             name = ''
         maintainer = dockerfile.labels.get('maintainer', '')
+
+        help_path = build_dir.path / self.help_file
 
         with open(help_path, 'r+') as help_file:
             lines = help_file.readlines()
@@ -90,9 +104,9 @@ class AddHelpPlugin(PreBuildPlugin):
 
                 self.log.info("added metadata to %s for generating nicer manpages", help_path)
 
-        man_path = os.path.join(self.workflow.df_dir, self.man_filename)
+        man_path = build_dir.path / self.man_filename
 
-        go_md2man_cmd = ['go-md2man', '-in={}'.format(help_path), '-out={}'.format(man_path)]
+        go_md2man_cmd = ['go-md2man', f'-in={help_path}', f'-out={man_path}']
 
         try:
             check_output(go_md2man_cmd, stderr=STDOUT)
@@ -110,11 +124,15 @@ class AddHelpPlugin(PreBuildPlugin):
                 )
             ) from e
 
-        if not os.path.exists(man_path):
+        if not man_path.exists():
             raise RuntimeError("go-md2man run complete, but man file is not found")
 
-        # Include the help file in the docker file
+        # We modified one file and created the other, let's copy both to all per-platform dirs
+        return [help_path, man_path]
 
+    def add_help_file_to_df(self, build_dir: BuildDir) -> None:
+        """Include the generated man file in the Dockerfile."""
+        dockerfile = build_dir.dockerfile
         lines = dockerfile.lines
 
         content = 'ADD {0} /{0}'.format(self.man_filename)
@@ -123,9 +141,4 @@ class AddHelpPlugin(PreBuildPlugin):
 
         dockerfile.lines = lines
 
-        self.log.info("added %s", man_path)
-
-        return {
-            'help_file': self.help_file,
-            'status': self.HELP_GENERATED
-        }
+        self.log.info("added %s", build_dir.path / self.man_filename)
