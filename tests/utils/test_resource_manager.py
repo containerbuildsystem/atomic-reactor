5fcb39e94af95d52904c5b0d0b2b80abddca7825
Tomas Veverka <tveverka@redhat.com>
2022-02-01 11:02:32 +0100

    ADD test_host_read_info_data, FIX init test

--- tests/utils/test_resource_manager.py
+++ tests/utils/test_resource_manager.py
@@ -5,11 +5,21 @@ All rights reserved.
 This software may be modified and distributed under the terms
 of the BSD license. See the LICENSE file for details.
 """
-from atomic_reactor.utils.resource_manager import Host, ResourceManager
 import pytest
+from unittest import mock
+from atomic_reactor.utils.resource_manager import Host, ResourceManager
 
 
 def test_host_init():
     my_host = Host('user', 'hostname')
-    assert my_host.username = 'user'
-    assert my_host.hostname = 'hostname'
+    assert my_host.username == 'user'
+    assert my_host.hostname == 'hostname'
+    assert my_host.slots == []
+    assert my_host.free_slots == []
+
+
+def test_host_read_info_data():
+    HOST_INFO = '{"hostname": "tst", "arch": "x86", "max_slot_count": 0}'
+    with mock.patch.object(Host, '_ssh_run_remote_command', HOST_INFO) as mock_obj:
+        my_host = Host('user', 'hostname')
+        assert my_host.check_avail() == []
