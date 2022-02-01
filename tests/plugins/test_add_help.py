c59bbc3dcf9daffa7e3dd09b8f5dd588cd68003f
Adam Cmiel <acmiel@redhat.com>
2022-02-01 11:14:10 +0100

    Update add_help plugin for Tekton
    CLOUDBLD-8123
    
    Copy the modified help file and generated man file to all build dirs.
    Update the Dockerfile in each build dir.
    
    Update tests accordingly, also take the opportunity to make the mocking
    more consistent.
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- tests/plugins/test_add_help.py
+++ tests/plugins/test_add_help.py
@@ -6,17 +6,20 @@ This software may be modified and distributed under the terms
 of the BSD license. See the LICENSE file for details.
 """
 
-import os
 import subprocess
 import pytest
 from datetime import datetime as dt
+from pathlib import Path
 from textwrap import dedent
+from typing import Dict, Optional, Callable, NamedTuple, List
+
 from flexmock import flexmock
 
-from atomic_reactor.plugin import PreBuildPluginsRunner
+from atomic_reactor.plugin import PluginsRunner
 from atomic_reactor.plugins.pre_add_help import AddHelpPlugin
-from atomic_reactor.util import df_parser
 from atomic_reactor import start_time as atomic_reactor_start_time
+
+from tests.mock_env import MockEnv
 
 
 class MockedPopen(object):
@@ -36,73 +39,117 @@ class MockedPopen(object):
         return ('', '')
 
 
-def generate_a_file(destpath, contents):
-    with open(destpath, 'w') as f:
-        f.write(dedent(contents))
+class MD2ManArgs(NamedTuple):
+    in_path: Path
+    out_path: Path
+
+
+def parse_md2man_args(cmd: List[str]) -> MD2ManArgs:
+    in_opt = "-in="
+    out_opt = "-out="
+
+    assert len(cmd) == 3
+    assert cmd[0] == "go-md2man"
+    assert cmd[1].startswith(in_opt)
+    assert cmd[2].startswith(out_opt)
+
+    in_path = cmd[1][len(in_opt):]
+    out_path = cmd[2][len(out_opt):]
+    return MD2ManArgs(Path(in_path), Path(out_path))
+
+
+class HelpMdFile(NamedTuple):
+    name: str
+    content: str
+
+
+def mock_md2man_success(out_content: str) -> Callable[..., MockedPopen]:
+    def mocked_md2man(*args, **kwargs):
+        cmd = args[0]
+        _, out_path = parse_md2man_args(cmd)
+        out_path.write_text(out_content)
+        return MockedPopen()
+
+    return mocked_md2man
+
+
+def mock_env(
+    workflow,
+    *,
+    df_content: str,
+    help_md: Optional[HelpMdFile] = None,
+    plugin_args: Optional[Dict[str, str]] = None,
+    mock_md2man: Callable[..., MockedPopen] = mock_md2man_success("man file content"),
+) -> PluginsRunner:
+
+    env = MockEnv(workflow).for_plugin("prebuild", AddHelpPlugin.key, plugin_args)
+    source_dir = Path(workflow.source.path)
+
+    (source_dir / "Dockerfile").write_text(df_content)
+    if help_md:
+        (source_dir / help_md.name).write_text(help_md.content)
+
+    workflow.build_dir.init_build_dirs(["aarch64", "x86_64"], workflow.source)
+
+    if help_md:
+        def check_popen(*args, **kwargs):
+            cmd = args[0]
+            in_path, out_path = parse_md2man_args(cmd)
+
+            assert in_path.name == help_md.name
+            assert out_path.name == AddHelpPlugin.man_filename
+
+            return mock_md2man(*args, **kwargs)
+
+        (flexmock(subprocess)
+         .should_receive("Popen")
+         .once()
+         .replace_with(check_popen))
+    else:
+        (flexmock(subprocess)
+         .should_receive("Popen")
+         .never())
+
+    return env.create_runner()
 
 
 @pytest.mark.parametrize('filename', ['help.md', 'other_file.md'])  # noqa
-def test_add_help_plugin(tmpdir, workflow, filename):
-    df_content = dedent("""
+def test_add_help_plugin(workflow, filename):
+    df_content = dedent(
+        """
         FROM fedora
         RUN yum install -y python-django
-        CMD blabla""")
-    df = df_parser(str(tmpdir))
-    df.content = df_content
+        CMD blabla
+        """
+    )
 
-    flexmock(workflow, df_path=df.dockerfile_path)
-    workflow.df_dir = str(tmpdir)
-
-    help_markdown_path = os.path.join(workflow.df_dir, filename)
-    generate_a_file(help_markdown_path, "foo")
-    help_man_path = os.path.join(workflow.df_dir, AddHelpPlugin.man_filename)
-    generate_a_file(help_man_path, "bar")
-
-    cmd = ['go-md2man', '-in={}'.format(help_markdown_path), '-out={}'.format(help_man_path)]
-
-    def check_popen(*args, **kwargs):
-        assert args[0] == cmd
-        return MockedPopen()
-
-    (flexmock(subprocess)
-     .should_receive("Popen")
-     .once()
-     .replace_with(check_popen))
-
-    runner = PreBuildPluginsRunner(
+    runner = mock_env(
         workflow,
-        [{
-            'name': AddHelpPlugin.key,
-            'args': {'help_file': filename}
-        }]
+        df_content=df_content,
+        help_md=HelpMdFile(filename, "markdown file content"),
+        plugin_args={"help_file": filename},
+        mock_md2man=mock_md2man_success("man file content")
     )
     runner.run()
 
-    assert df.content == dedent("""
-        FROM fedora
-        RUN yum install -y python-django
-        ADD %s /%s
-        CMD blabla""" % (AddHelpPlugin.man_filename, AddHelpPlugin.man_filename))
+    def check_df_and_man_file(build_dir):
+        assert build_dir.dockerfile_path.read_text() == dedent(
+            f"""
+            FROM fedora
+            RUN yum install -y python-django
+            ADD {AddHelpPlugin.man_filename} /{AddHelpPlugin.man_filename}
+            CMD blabla
+            """
+        )
+        assert (build_dir.path / AddHelpPlugin.man_filename).read_text() == "man file content"
 
+    workflow.build_dir.for_each_platform(check_df_and_man_file)
     assert workflow.annotations['help_file'] == filename
 
 
 @pytest.mark.parametrize('filename', ['help.md', 'other_file.md'])  # noqa
-def test_add_help_no_help_file(workflow, tmpdir, filename):
-    df_content = "FROM fedora"
-    df = df_parser(str(tmpdir))
-    df.content = df_content
-
-    flexmock(workflow, df_path=df.dockerfile_path)
-    workflow.df_dir = str(tmpdir)
-
-    runner = PreBuildPluginsRunner(
-        workflow,
-        [{
-            'name': AddHelpPlugin.key,
-            'args': {'help_file': filename}
-        }]
-    )
+def test_add_help_no_help_file(workflow, filename):
+    runner = mock_env(workflow, df_content="FROM fedora")
     # Runner should not crash if no help.md found
     result = runner.run()
     assert result == {'add_help': {
@@ -115,68 +162,36 @@ def test_add_help_no_help_file(workflow, tmpdir, filename):
 @pytest.mark.parametrize('go_md2man_result', [
     'binary_missing', 'input_missing', 'other_os_error',
     'result_missing', 'fail', 'pass'])
-def test_add_help_md2man_error(workflow, tmpdir, filename, go_md2man_result, caplog):
-    df_content = "FROM fedora"
-    df = df_parser(str(tmpdir))
-    df.content = df_content
-
-    flexmock(workflow, df_path=df.dockerfile_path)
-    workflow.df_dir = str(tmpdir)
-
-    help_markdown_path = os.path.join(workflow.df_dir, filename)
+def test_add_help_md2man_error(workflow, filename, go_md2man_result):
     if go_md2man_result != 'input_missing':
-        generate_a_file(help_markdown_path, "foo")
-    help_man_path = os.path.join(workflow.df_dir, AddHelpPlugin.man_filename)
-    if go_md2man_result != 'result_missing':
-        generate_a_file(help_man_path, "bar")
+        help_md = HelpMdFile(filename, "markdown file content")
+    else:
+        help_md = None
 
-    cmd = ['go-md2man',
-           '-in={}'.format(help_markdown_path),
-           '-out={}'.format(help_man_path)]
-
-    def check_popen_pass(*args, **kwargs):
-        assert args[0] == cmd
-        return MockedPopen()
-
-    def check_popen_binary_missing(*args, **kwargs):
-        check_popen_pass(*args, **kwargs)
+    def md2man_binary_missing(*args, **kwargs):
         raise OSError(2, "No such file or directory")
 
-    def check_popen_other_os_error(*args, **kwargs):
-        check_popen_pass(*args, **kwargs)
+    def md2man_other_os_error(*args, **kwargs):
         raise OSError(0, "Other error")
 
-    def check_popen_fail(*args, **kwargs):
-        check_popen_pass(*args, **kwargs)
+    def md2man_fail(*args, **kwargs):
         raise subprocess.CalledProcessError(returncode=1, cmd=args[0])
 
-    if go_md2man_result == 'binary_missing':
-        (flexmock(subprocess)
-         .should_receive("Popen")
-         .once()
-         .replace_with(check_popen_binary_missing))
-    elif go_md2man_result == 'other_os_error':
-        (flexmock(subprocess)
-         .should_receive("Popen")
-         .once()
-         .replace_with(check_popen_other_os_error))
-    elif go_md2man_result == 'fail':
-        (flexmock(subprocess)
-         .should_receive("Popen")
-         .once()
-         .replace_with(check_popen_fail))
-    elif go_md2man_result in ['pass', 'result_missing']:
-        (flexmock(subprocess)
-         .should_receive("Popen")
-         .once()
-         .replace_with(check_popen_pass))
+    def md2man_no_output(*args, **kwargs):
+        return MockedPopen()
 
-    runner = PreBuildPluginsRunner(
+    runner = mock_env(
         workflow,
-        [{
-            'name': AddHelpPlugin.key,
-            'args': {'help_file': filename}
-        }]
+        df_content="FROM fedora",
+        help_md=help_md,
+        plugin_args={"help_file": filename},
+        mock_md2man=(
+            md2man_binary_missing if go_md2man_result == "binary_missing"
+            else md2man_other_os_error if go_md2man_result == "other_os_error"
+            else md2man_fail if go_md2man_result == "fail"
+            else md2man_no_output if go_md2man_result == "result_missing"
+            else mock_md2man_success("man file content")
+        )
     )
 
     result = runner.run()
@@ -221,51 +236,33 @@ def test_add_help_md2man_error(workflow, tmpdir, filename, go_md2man_result, cap
 
 
 @pytest.mark.parametrize('filename', ['help.md', 'other_file.md'])  # noqa
-def test_add_help_generate_metadata(tmpdir, workflow, filename):
+def test_add_help_generate_metadata(workflow, filename):
     df_content = dedent("""\
         FROM fedora
         LABEL name='test' \\
               maintainer='me'
         """)
 
-    df = df_parser(str(tmpdir))
-    df.content = df_content
-
-    flexmock(workflow, df_path=df.dockerfile_path)
-    workflow.df_dir = str(tmpdir)
-
-    help_markdown_path = os.path.join(workflow.df_dir, filename)
-    generate_a_file(help_markdown_path, "foo")
-    help_man_path = os.path.join(workflow.df_dir, AddHelpPlugin.man_filename)
-    generate_a_file(help_man_path, "bar")
-
-    cmd = ['go-md2man', '-in={}'.format(help_markdown_path), '-out={}'.format(help_man_path)]
-
-    def check_popen(*args, **kwargs):
-        assert args[0] == cmd
-        return MockedPopen()
-
-    (flexmock(subprocess)
-     .should_receive("Popen")
-     .once()
-     .replace_with(check_popen))
-
-    runner = PreBuildPluginsRunner(
+    runner = mock_env(
         workflow,
-        [{
-            'name': AddHelpPlugin.key,
-            'args': {'help_file': filename}
-        }]
+        df_content=df_content,
+        help_md=HelpMdFile(filename, "markdown file content\n"),
+        plugin_args={"help_file": filename},
     )
     runner.run()
-    lines = ""
-    with open(help_markdown_path) as f:
-        lines = "".join(f.readlines())
 
-    example = dedent("""\
-        %% test (1) Container Image Pages
-        %% me
-        %% %s
-        foo""") % dt.fromtimestamp(atomic_reactor_start_time).strftime(format="%B %-d, %Y")
+    expect_content = dedent(
+        f"""\
+        % test (1) Container Image Pages
+        % me
+        % {dt.fromtimestamp(atomic_reactor_start_time).strftime(format="%B %-d, %Y")}
+        markdown file content
+        """
+    )
 
-    assert lines == dedent(example)
+    def check_help_md(build_dir):
+        help_md_path = build_dir.path / filename
+        content = help_md_path.read_text()
+        assert content == expect_content
+
+    workflow.build_dir.for_each_platform(check_help_md)
