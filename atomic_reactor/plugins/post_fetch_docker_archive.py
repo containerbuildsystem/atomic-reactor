0ef91305f1d8a4e7539e6723523b8e6db4ced429
Harsh Modi <hmodi@redhat.com>
2022-02-27 19:41:36 -0500

    download images for each platform into platform specific directory
    * CLOUDBLD-8248
    
    Signed-off-by: Harsh Modi <hmodi@redhat.com>

--- atomic_reactor/plugins/post_fetch_docker_archive.py
+++ atomic_reactor/plugins/post_fetch_docker_archive.py
@@ -5,11 +5,8 @@ All rights reserved.
 This software may be modified and distributed under the terms
 of the BSD license. See the LICENSE file for details.
 """
-
-import os
-
-from atomic_reactor.constants import (EXPORTED_COMPRESSED_IMAGE_NAME_TEMPLATE,
-                                      IMAGE_TYPE_DOCKER_ARCHIVE)
+from atomic_reactor.constants import IMAGE_TYPE_DOCKER_ARCHIVE
+from atomic_reactor.dirs import BuildDir
 from atomic_reactor.plugin import PostBuildPlugin
 from atomic_reactor.util import get_exported_image_metadata, is_scratch_build
 
@@ -25,6 +22,20 @@ class FetchDockerArchivePlugin(PostBuildPlugin):
         super(FetchDockerArchivePlugin, self).__init__(workflow)
         self.source_build = bool(self.workflow.data.build_result.source_docker_archive)
 
+    def download_image(self, build_dir: BuildDir):
+        image = self.workflow.data.tag_conf.get_unique_images_with_platform(build_dir.platform)[0]
+        image_path = str(build_dir.exported_squashed_image)
+        image_type = IMAGE_TYPE_DOCKER_ARCHIVE
+
+        self.log.info('fetching image %s', image)
+        self.workflow.imageutil.download_image_archive_tarball(image, image_path)
+
+        metadata = get_exported_image_metadata(image_path, image_type)
+
+        self.log.info('image for platform:%s available at %s', build_dir.platform, image_path)
+
+        return metadata
+
     def run(self):
         if is_scratch_build(self.workflow):
             # required only to make an archive for Koji
@@ -34,17 +45,5 @@ class FetchDockerArchivePlugin(PostBuildPlugin):
         if self.source_build:
             self.log.info('skipping, no exported source image')
             return
-        image = self.workflow.image
-        image_type = IMAGE_TYPE_DOCKER_ARCHIVE
-        self.log.info('fetching image %s from docker', image)
-        # OSBS2 TBD
-        # Set image_tarball_path, if image tarball is not yet
-        # stored somewhere, download it
-        # with imageutil.download_image_archive_tarball() for each platform
-        outfile = os.path.join(self.workflow.source.workdir,
-                               EXPORTED_COMPRESSED_IMAGE_NAME_TEMPLATE)
-        os.mknod(outfile)
-        metadata = get_exported_image_metadata(outfile, image_type)
 
-        self.workflow.data.exported_image_sequence.append(metadata)
-        self.log.info('image is available as %s', outfile)
+        return self.workflow.build_dir.for_each_platform(self.download_image)
--- tests/plugins/test_fetch_docker_archive.py
+++ tests/plugins/test_fetch_docker_archive.py
@@ -1,23 +1,35 @@
 import os
-import tarfile
 
 import pytest
+from flexmock import flexmock
 
-from atomic_reactor.constants import (EXPORTED_COMPRESSED_IMAGE_NAME_TEMPLATE,
-                                      IMAGE_TYPE_DOCKER_ARCHIVE)
+from atomic_reactor.constants import EXPORTED_SQUASHED_IMAGE_NAME, IMAGE_TYPE_DOCKER_ARCHIVE
+from atomic_reactor.dirs import BuildDir
+from atomic_reactor.inner import BuildResult
 from atomic_reactor.plugin import PostBuildPluginsRunner
 from atomic_reactor.plugins.post_fetch_docker_archive import FetchDockerArchivePlugin
-from atomic_reactor.inner import BuildResult
+from atomic_reactor.utils.imageutil import ImageUtil
 
 
 class TestFetchDockerArchive(object):
+
+    def create_image(self, build_dir: BuildDir):
+        exp_img = build_dir.path / 'image.tar'
+        with open(exp_img, 'w') as f:
+            f.write('test')
+
     @pytest.mark.parametrize('source_build', (True, False))
     def test_fetch_docker_archive(self, tmpdir, caplog, workflow, source_build):
-        exp_img = os.path.join(str(tmpdir), 'img.tar')
-
         if source_build:
             workflow.data.build_result = BuildResult(source_docker_archive="oci_path")
         else:
+            platforms = ['x86_64', 's390x', 'ppc64le', 'aarch64']
+
+            workflow.build_dir.init_build_dirs(platforms, workflow.source)
+            workflow.data.tag_conf.add_unique_image('registry.com/image:latest')
+
+            workflow.build_dir.for_each_platform(self.create_image)
+            flexmock(ImageUtil).should_receive('download_image_archive_tarball').times(4)
             workflow.data.build_result = BuildResult(image_id="12345")
 
         runner = PostBuildPluginsRunner(
@@ -29,18 +41,19 @@ class TestFetchDockerArchive(object):
             }]
         )
 
-        runner.run()
+        results = runner.run()
 
         if source_build:
             assert 'skipping, no exported source image' in caplog.text
         else:
-            img = os.path.join(
-                workflow.source.workdir,
-                EXPORTED_COMPRESSED_IMAGE_NAME_TEMPLATE)
-            assert os.path.exists(img)
-            metadata = workflow.data.exported_image_sequence[-1]
-            assert metadata['path'] == img
-            assert metadata['type'] == IMAGE_TYPE_DOCKER_ARCHIVE
+            for platform, metadata in results[FetchDockerArchivePlugin.key].items():
+                image_path = workflow.build_dir.path / platform
+                img = os.path.join(image_path / EXPORTED_SQUASHED_IMAGE_NAME)
+                assert os.path.exists(img)
+                assert metadata['path'] == img
+                assert metadata['type'] == IMAGE_TYPE_DOCKER_ARCHIVE
+                assert f'image for platform:{platform} available at ' \
+                       f"{image_path / 'image.tar'}" in caplog.text
 
     def test_skip_plugin(self, caplog, workflow):
         workflow.user_params['scratch'] = True
