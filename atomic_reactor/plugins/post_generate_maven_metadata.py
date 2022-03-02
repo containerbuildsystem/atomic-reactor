a3799fa00bcf81c5b0e7e7530adb93a5b66e21d7
Harsh Modi <hmodi@redhat.com>
2022-03-02 14:31:45 -0500

    move most of the maven metadata processing to fetch_maven_artifacts
    generate_maven_metadata will only generate
    remote_source_files to be uploaded to brew.
    
    This is to avoid reprocessing config files.
    Metadata generation and fetching of artifacts
    is now done in a single pass. Only remote source
    files are fetched and processed later.
    
    * CLOUDBLD-8264
    
    Signed-off-by: Harsh Modi <hmodi@redhat.com>

--- atomic_reactor/plugins/post_generate_maven_metadata.py
+++ atomic_reactor/plugins/post_generate_maven_metadata.py
@@ -1,24 +1,19 @@
 """
-Copyright (c) 2021 Red Hat, Inc
+Copyright (c) 2021-2022 Red Hat, Inc
 All rights reserved.
 
 This software may be modified and distributed under the terms
 of the BSD license. See the LICENSE file for details.
 """
-import hashlib
 import os
 import re
 from collections import namedtuple
 
-import koji
-
 from atomic_reactor import util
-from atomic_reactor.constants import (KOJI_BTYPE_REMOTE_SOURCE_FILE,
+from atomic_reactor.constants import (KOJI_BTYPE_REMOTE_SOURCE_FILE, PLUGIN_FETCH_MAVEN_KEY,
                                       PLUGIN_GENERATE_MAVEN_METADATA_KEY)
-from atomic_reactor.config import get_koji_session
 from atomic_reactor.download import download_url
 from atomic_reactor.plugin import PostBuildPlugin
-from atomic_reactor.utils.koji import NvrRequest
 
 DownloadRequest = namedtuple('DownloadRequest', 'url dest checksums')
 
@@ -37,98 +32,15 @@ class GenerateMavenMetadataPlugin(PostBuildPlugin):
         :param workflow: DockerBuildWorkflow instance
         """
         super(GenerateMavenMetadataPlugin, self).__init__(workflow)
-        self.session = None
-        self.workdir = self.workflow.source.get_build_file_path()[1]
-        self.no_source_artifacts = []
         self.source_url_to_artifacts = {}
 
-    def get_nvr_components(self, nvr_requests):
-        components = []
-
-        for nvr_request in nvr_requests:
-            # We're assuming here that we won't run into any errors here
-            #  since this plugin runs after pre_fetch_maven_artifacts
-            #  that should fail if there were any errors.
-            build_info = self.session.getBuild(nvr_request.nvr)
-            build_archives = self.session.listArchives(buildID=build_info['id'],
-                                                       type='maven')
-            build_archives = nvr_request.match_all(build_archives)
-
-            for build_archive in build_archives:
-                checksum_type = koji.CHECKSUM_TYPES[build_archive['checksum_type']]
-                components.append({
-                    'type': 'kojifile',
-                    'filename': build_archive['filename'],
-                    'filesize': build_archive['size'],
-                    'checksum': build_archive['checksum'],
-                    'checksum_type': checksum_type,
-                    'nvr': nvr_request.nvr,
-                    'archive_id': build_archive['id'],
-                })
-
-        return components
-
-    def get_pnc_build_metadata(self, pnc_requests):
-        builds = pnc_requests.get('builds', [])
-
-        if not builds:
-            return {}
-
-        pnc_build_metadata = {'builds': []}
-
-        for build in builds:
-            pnc_build_metadata['builds'].append({'id': build['build_id']})
-
-        return pnc_build_metadata
-
-    def process_url_requests(self, url_requests):
-        download_queue = []
-
-        for url_request in url_requests:
-            artifact_checksums = {algo: url_request[algo] for algo in
-                                  hashlib.algorithms_guaranteed
-                                  if algo in url_request}
-
-            artifact = {
-                'url': url_request['url'],
-                'checksums': artifact_checksums,
-                'filename': os.path.basename(url_request['url'])
-            }
-
-            if 'source-url' not in url_request:
-                self.no_source_artifacts.append(artifact)
-                msg = f"No source-url found for {url_request['url']}.\n"
-                self.log.warning(msg)
-                msg += 'fetch-artifacts-url without source-url is deprecated\n'
-                msg += 'to fix this please provide the source-url according to ' \
-                       'https://osbs.readthedocs.io/en/latest/users.html#fetch-artifacts-url-yaml'
-                self.log.user_warning(msg)
-                continue
-
-            source_url = url_request['source-url']
-
-            checksums = {algo: url_request[('source-' + algo)] for algo in
-                         hashlib.algorithms_guaranteed
-                         if ('source-' + algo) in url_request}
-
-            if source_url not in self.source_url_to_artifacts:
-                self.source_url_to_artifacts[source_url] = [artifact]
-                # source_url will mostly be gerrit URLs that don't have filename
-                #  in the URL itself so we'll have to get filename from URL response
-                target = os.path.basename(source_url)
-
-                download_queue.append(DownloadRequest(source_url, target, checksums))
-            else:
-                self.source_url_to_artifacts[source_url].append(artifact)
-        return download_queue
-
-    def download_sources(self, download_queue):
+    def get_remote_source_files(self, download_queue):
         remote_source_files = []
-        downloads_path = os.path.join(self.workdir, self.DOWNLOAD_DIR)
+        downloads_path = self.workflow.build_dir.any_platform.path / self.DOWNLOAD_DIR
 
         session = util.get_retrying_requests_session()
 
-        self.log.debug('%d files to download', len(download_queue))
+        self.log.debug('%d url source files to download', len(download_queue))
 
         koji_config = self.workflow.conf.koji
         insecure = koji_config.get('insecure_download', False)
@@ -177,22 +89,9 @@ class GenerateMavenMetadataPlugin(PostBuildPlugin):
         """
         Run the plugin.
         """
+        fetch_maven_result = self.workflow.data.prebuild_results[PLUGIN_FETCH_MAVEN_KEY]
+        source_download_queue = fetch_maven_result['source_download_queue']
+        self.source_url_to_artifacts = fetch_maven_result['source_url_to_artifacts']
+        remote_source_files = self.get_remote_source_files(source_download_queue)
 
-        self.session = get_koji_session(self.workflow.conf)
-
-        nvr_requests = [
-            NvrRequest(**nvr_request) for nvr_request in
-            util.read_fetch_artifacts_koji(self.workflow) or []
-        ]
-        pnc_requests = util.read_fetch_artifacts_pnc(self.workflow) or {}
-        url_requests = util.read_fetch_artifacts_url(self.workflow) or []
-
-        components = self.get_nvr_components(nvr_requests)
-        pnc_build_metadata = self.get_pnc_build_metadata(pnc_requests)
-        download_queue = self.process_url_requests(url_requests)
-        remote_source_files = self.download_sources(download_queue)
-
-        return {'components': components,
-                'no_source': self.no_source_artifacts,
-                'pnc_build_metadata': pnc_build_metadata,
-                'remote_source_files': remote_source_files}
+        return {'remote_source_files': remote_source_files}
