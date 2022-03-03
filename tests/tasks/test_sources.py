ca6b41054fd83f2cb625e66df0bdfa97f5f0bbbb
Adam Cmiel <acmiel@redhat.com>
2022-03-03 10:40:16 +0100

    Split plugin task logic: prep workflow, execute
    The execute() method of a plugin based task should hardly ever have a
    reason to change in subclasses, but the part that prepares the workflow
    instance might. For example, source container tasks partially customized
    workflow preparation by overriding the get_build_dir() method.
    
    Instead of overriding get_build_dir(), add a prepare_workflow() method
    that allows subclasses to customize any part of workflow preparation.
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- tests/tasks/test_sources.py
+++ tests/tasks/test_sources.py
@@ -6,23 +6,40 @@ This software may be modified and distributed under the terms
 of the BSD license. See the LICENSE file for details.
 """
 
+import pytest
+
 from atomic_reactor import source
 from atomic_reactor.tasks import sources
+
+
+@pytest.fixture
+def params(build_dir) -> sources.SourceBuildTaskParams:
+    return sources.SourceBuildTaskParams(
+        # build_dir has to be an existing directory because DummySource creates a subdirectory
+        #   as soon as an instance is created
+        build_dir=str(build_dir),
+        context_dir="/context",
+        config_file="config.yaml",
+        user_params={},
+    )
 
 
 class TestSourceBuildTaskParams:
     """Tests for the SourceBuildTaskParams class."""
 
-    def test_source_property(self, tmpdir):
-        params = sources.SourceBuildTaskParams(
-            # build_dir has to be an existing directory because DummySource creates a subdirectory
-            #   as soon as an instance is created
-            build_dir=str(tmpdir),
-            context_dir="/context",
-            config_file="config.yaml",
-            user_params={},
-        )
+    def test_source_property(self, params):
         src = params.source
 
         assert isinstance(src, source.DummySource)
-        assert src.workdir == str(tmpdir)
+        assert src.workdir == params.build_dir
+
+
+class TestSourceBuildTask:
+    """Tests for the SourceBuildTask class."""
+
+    def test_prepare_workflow(self, params):
+        task = sources.SourceBuildTask(params)
+
+        workflow = task.prepare_workflow()
+        assert workflow.build_dir.platforms == ["noarch"]
+        assert workflow.build_dir.has_sources
