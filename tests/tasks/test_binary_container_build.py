48e59c95003578cabc9e6ec7c0707afea1c6a434
Adam Cmiel <acmiel@redhat.com>
2022-03-04 14:50:56 +0100

    Add subprocess handling to build task
    CLOUDBLD-8657
    
    To get closer to the desired state, instead of doing absolutely nothing
    in the build_container() and push_container() methods, make them call
    subprocesses that... still do nothing.
    
    However, the path from here to a functional build task should be short.
    Just replace the build_cmd and push_cmd with actual podman commands, and
    we should be good to go.
    
    The subprocess polling loop is based on the build_imagebuilder plugin
    from OSBS 1.
    
    Signed-off-by: Adam Cmiel <acmiel@redhat.com>

--- tests/tasks/test_binary_container_build.py
+++ tests/tasks/test_binary_container_build.py
@@ -6,9 +6,12 @@ This software may be modified and distributed under the terms
 of the BSD license. See the LICENSE file for details.
 """
 
+import io
+import re
+import subprocess
 from pathlib import Path
 from textwrap import dedent
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 from osbs.utils import ImageName
 
@@ -18,7 +21,13 @@ from flexmock import flexmock
 from atomic_reactor import dirs
 from atomic_reactor import inner
 from atomic_reactor.constants import PLUGIN_CHECK_AND_SET_PLATFORMS_KEY
-from atomic_reactor.tasks.binary_container_build import BinaryBuildTask, BinaryBuildTaskParams
+from atomic_reactor.tasks.binary_container_build import (
+    BinaryBuildTask,
+    BinaryBuildTaskParams,
+    BuildProcessError,
+    PushError,
+)
+from atomic_reactor.utils import retries
 
 CONTEXT_DIR = "/workspace/ws-context-dir"
 CONFIG_PATH = "/etc/atomic-reactor/config.yaml"
@@ -83,6 +92,34 @@ def mock_workflow_data(*, enabled_platforms: List[str]) -> inner.ImageBuildWorkf
     return mocked_data
 
 
+class MockedPopen:
+    def __init__(self, rc: int, output_lines: List[str]):
+        self._rc = rc
+        self.stdout = io.StringIO("".join(output_lines))
+
+    def poll(self):
+        if self.stdout.tell() >= len(self.stdout.getvalue()):
+            # no more output, the process has ended
+            return self._rc
+        return None
+
+
+def mock_popen(
+    rc: int, output_lines: List[str], expect_cmd: Optional[List[str]] = None
+) -> None:
+    """Make subprocess.Popen returned a mocked Popen.
+
+    The output_lines should end with '\n', otherwise they wil be combined to a single line.
+    Optionally, pass in the expected command to be checked.
+    """
+    def popen(cmd, *args, **kwargs):
+        if expect_cmd:
+            assert cmd == expect_cmd
+        return MockedPopen(rc, output_lines)
+
+    flexmock(subprocess).should_receive("Popen").replace_with(popen)
+
+
 class TestBinaryBuildTask:
     """Tests from the BinaryBuildTask class."""
 
@@ -129,3 +166,84 @@ class TestBinaryBuildTask:
         assert "output line 1" in caplog.text
         assert "output line 2" in caplog.text
         assert DOCKERFILE_CONTENT in caplog.text
+
+    def test_print_dockerfile_on_failure(self, x86_task_params, x86_build_dir, caplog):
+        mock_workflow_data(enabled_platforms=["x86_64"])
+        x86_build_dir.dockerfile_path.write_text(DOCKERFILE_CONTENT)
+
+        (
+            flexmock(BinaryBuildTask)
+            .should_receive("build_container")
+            .and_raise(BuildProcessError("something went wrong"))
+        )
+
+        task = BinaryBuildTask(x86_task_params)
+        with pytest.raises(BuildProcessError):
+            task.execute()
+
+        assert DOCKERFILE_CONTENT in caplog.text
+
+    def test_build_container(self, x86_task_params, x86_build_dir):
+        # TBD: add the actual expect_cmd later
+        mock_popen(0, ["starting the build\n", "finished successfully\n"], expect_cmd=None)
+
+        task = BinaryBuildTask(x86_task_params)
+        output_lines = task.build_container(
+            build_dir=x86_build_dir,
+            build_args=BUILD_ARGS,
+            dest_tag=X86_UNIQUE_IMAGE,
+        )
+
+        assert list(output_lines) == ["starting the build\n", "finished successfully\n"]
+
+    @pytest.mark.parametrize(
+        "output_lines, expect_err_line",
+        [
+            (["starting the build\n", "failed :(\n"], "failed :("),
+            ([], "<no output!>"),
+        ]
+    )
+    def test_build_container_fails(
+        self, output_lines, expect_err_line, x86_task_params, x86_build_dir
+    ):
+        mock_popen(1, output_lines)
+
+        task = BinaryBuildTask(x86_task_params)
+        returned_lines = task.build_container(
+            build_dir=x86_build_dir,
+            build_args=BUILD_ARGS,
+            dest_tag=X86_UNIQUE_IMAGE,
+        )
+
+        for expect_line in output_lines:
+            assert next(returned_lines) == expect_line
+
+        err_msg = rf"Build failed \(rc=1\). {re.escape(expect_err_line)}"
+
+        with pytest.raises(BuildProcessError, match=err_msg):
+            next(returned_lines)
+
+    def test_push_container(self, x86_task_params):
+        (
+            flexmock(retries)
+            .should_receive("run_cmd")
+            .with_args(["echo", str(X86_UNIQUE_IMAGE)])  # TBD: change me later
+            .once()
+        )
+
+        task = BinaryBuildTask(x86_task_params)
+        task.push_container(X86_UNIQUE_IMAGE)
+
+    def test_push_container_fails(self, x86_task_params):
+        (
+            flexmock(retries)
+            .should_receive("run_cmd")
+            .and_raise(subprocess.CalledProcessError(1, 'some command'))
+        )
+
+        task = BinaryBuildTask(x86_task_params)
+
+        err_msg = r"Push failed \(rc=1\). Check the logs for more details."
+
+        with pytest.raises(PushError, match=err_msg):
+            task.push_container(X86_UNIQUE_IMAGE)
