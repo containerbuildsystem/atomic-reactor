9aec33f2f442a561e0d94a21c61318034a2b5329
mkosiarc <mkosiarc@redhat.com>
2022-11-09 09:58:27 +0100

    Bump flatpak-module-tools version
    The reason is that so we can override the architecture of the flatpak
    build
    https://pagure.io/flatpak-module-tools/pull-request/16
    
    CLOUDBLD-11371
    
    Signed-off-by: mkosiarc <mkosiarc@redhat.com>

--- setup.py
+++ setup.py
@@ -27,8 +27,7 @@ setup(
     install_requires=[
         'backoff',
         'dockerfile-parse>=0.0.13',
-        'flatpak-module-tools >= 0.11,!=0.13,<=0.13.1;python_version<"3.9"',
-        'flatpak-module-tools >= 0.11;python_version>="3.9"',
+        'flatpak-module-tools>=0.14',
         'jsonschema',
         'paramiko>=2.10.1',
         'PyYAML',
