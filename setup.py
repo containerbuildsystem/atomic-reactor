a4dde6ef74618e8bf4b37cec3a42ae7d121b99bd
Robert Cerven <rcerven@redhat.com>
2024-11-26 20:56:29 +0100

    Automatic commit of package [atomic-reactor] release [4.14.0-1].
    Signed-off-by: Robert Cerven <rcerven@redhat.com>

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ HOMEPAGE = "https://github.com/containerbuildsystem/atomic-reactor"
 
 setup(
     name='atomic-reactor',
-    version='4.13.0',
+    version='4.14.0',
     description=DESCRIPTION,
     author='Red Hat, Inc.',
     author_email='atomic-devel@projectatomic.io',
